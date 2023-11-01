661b18b1cb84963bf6c1042eab52775b7542e72b
kkj2010 <kunjukim0210@gmail.com>
2023-11-01 01:15:16 -0400

    set up layout for sign in

--- fwb/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ fwb/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -3,12 +3,24 @@ import "./page.css";
 
 export default function Page() {
   return (
-    <div>
+    <div className="big">
       <div className="signinContainer">
-      <div className="leftSigninContainer"></div>
-      <div className="hi">
-        <SignIn />
+        <div className="circle1"></div>
+        <div className="circle2"></div>
+        <div className="two">
+        <div className="circle3"></div>
+        <div className="circle4"></div>
+        </div>
+        <div className="circle5"></div>
+        <div className="three">
+        <div className="circle6"></div>
+        <div className="circle7"></div>
+        </div>
       </div>
+      <div className="leftSigninContainer">
+        <div className="signin">
+          <SignIn />
+        </div>
       </div>
     </div>
   );
