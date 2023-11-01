a04a7aa4a711ac972db221ede80eb03b75bf3815
kkj2010 <kunjukim0210@gmail.com>
2023-10-31 20:06:45 -0400

    add css in main page

--- fwb/app/(landing)/page.tsx
+++ fwb/app/(landing)/page.tsx
@@ -3,24 +3,25 @@ import Link from "next/link";
 import "./page.css";
 const LandingPage = () => {
   return (
-    <div>
-      <h1 className="flex justify-center items-center text-yellow-200 font-bold">
+    <div className="top">
+      <h1 className="flex justify-center items-center text-yellow-200 font-bold"style={{ marginLeft: '10px' }}>
         FRIENDS WITH BENEFITS HOMEPAGE
       </h1>
-      <div className="flex justify-center items-center gap-x-2">
-        <div className="flex justify-center">
+      {/* <div className="flex justify-center items-center gap-x-2"> */}
+        <div className="exploreLists">
           <div className="exploreList">Explore</div>
           <div className="exploreList">Share Discounts</div>
           <div className="exploreList">About</div>
           <div className="exploreList">Groups</div>
         </div>
+        
         <Link href="/sign-in">
-          <Button variant="secondary">Start Now</Button>
+          <Button className="signinButton" variant="secondary">Start Now</Button>
         </Link>
         <Link href="/sign-up">
           <Button variant="secondary">Register</Button>
         </Link>
-      </div>
+      {/* </div> */}
     </div>
   );
 };
