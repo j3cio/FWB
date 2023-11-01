6470dc1833b90b89c4220549ad9bbc10bd978180
kkj2010 <kunjukim0210@gmail.com>
2023-11-01 00:03:18 -0400

    work on sign in page

--- fwb/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ fwb/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,15 @@
 import { SignIn } from "@clerk/nextjs";
-import "./page.css"
+import "./page.css";
 
 export default function Page() {
-  return <div className="hi"><SignIn /></div>;
+  return (
+    <div>
+      <div className="signinContainer">
+      <div className="leftSigninContainer"></div>
+      <div className="hi">
+        <SignIn />
+      </div>
+      </div>
+    </div>
+  );
 }
