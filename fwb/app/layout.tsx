b19388ee9180a3baceb916cf3e829f9d7f12e56d
kkj2010 <kunjukim0210@gmail.com>
2023-11-03 14:54:23 -0400

    work on main page

--- fwb/app/layout.tsx
+++ fwb/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
+import { dark } from '@clerk/themes';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,16 +11,38 @@ export const metadata: Metadata = {
   description: 'Discounts Have Never Been Easier To Find!',
 }
 
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode
+// }) {
+//   return (
+//     <ClerkProvider>
+//       <html lang="en">
+//         <body className={inter.className}>{children}</body>
+//       </html>
+//     </ClerkProvider>
+//   )
+// }
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark // dark 테마를 적용합니다.
+      }}
+    >
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body>{children}</body>
       </html>
     </ClerkProvider>
   )
 }
+
+
+
