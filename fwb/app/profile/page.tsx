d38e9159ea321517ce7954c33329c4cfd5e02b5d
Derick Young <young.derickj@gmail.com>
2023-11-03 13:56:38 -0600

    Create profile page route

---
+++ fwb/app/profile/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/ui/Navbar";
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div>Hello World!</div>
+    </div>
+  );
+}
+
+export default page;
