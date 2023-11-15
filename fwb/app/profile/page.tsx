e320ef5f4ea8a5ad5d9dd169e689febdbcc556d8
Derick Young <young.derickj@gmail.com>
2023-11-15 13:10:30 -0700

    Clean up code
    Fixed some naming and removed theme from profile page

--- fwb/app/profile/page.tsx
+++ fwb/app/profile/page.tsx
@@ -1,69 +1,32 @@
 "use client";
-
-import Navbar from "@/components/profile/Navbar";
 import { Button } from "@mui/material";
 import React from "react";
 import Image from "next/image";
-import imageSrc from "../../public/bargain1700x350.png";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Avatar from "@mui/material/Avatar";
-import SaveIcon from "../../components/profile/icons/save.svg";
-import BlueGroupIcon from "../../components/profile/icons/groups-blue.svg";
-import LinkedInIcon from "../../components/profile/icons/linkedin.svg";
-import ArrowForward from "@/components/profile/ArrowForward";
-import BlueArrowForward from "@/components/profile/YellowArrowForward";
-// Augment the palette to include an ochre color
-declare module "@mui/material/styles" {
-  interface Palette {
-    ochre: Palette["primary"];
-  }
-
-  interface PaletteOptions {
-    ochre?: PaletteOptions["primary"];
-  }
-}
-
-// Update the Button's color options to include an ochre option
-declare module "@mui/material/Button" {
-  interface ButtonPropsColorOverrides {
-    ochre: true;
-  }
-}
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#8e94e9",
-      light: "#ffffff",
-    },
-    ochre: {
-      main: "#1a1a23",
-      light: "#f6ff82",
-      dark: "#A29415",
-      contrastText: "#242105",
-    },
-  },
-});
+import Navbar from "@/components/profile/Navbar";
+import BargainBackgroundImage from "../../public/bargain1700x350.png";
+import AvatarIcon from "@mui/material/Avatar";
+import SaveIcon from "../../components/profile/icons/save.svg"; //
+import BlueGroupIcon from "../../components/profile/icons/groups-blue.svg";//
+import LinkedInIcon from "../../components/profile/icons/linkedin.svg";//
+import WhiteArrowForward from "@/components/profile/WhiteArrowForward";
+import BlueArrowForward from "@/components/profile/BlueArrowForward"
 
 function page() {
-  // Set up color theme
-  // Proper icons and logos
   // Need to update font
   // Make Bargain text responsive
   // Animations/Hover effects for buttons, etc..
   return (
-    <ThemeProvider theme={theme}>
       <div className="bg-[#1a1a23] min-h-screen">
         <Navbar></Navbar>
         {/*Container div*/}
         <div className="flex flex-1 flex-col h-full w-full items-center justify-center">
           {/*Profile div*/}
           <div className="flex w-11/12 h-1/5 mt-6 mb-6 gap-10 border-b-2 border-slate-200 pb-6">
-            <Avatar
+            <AvatarIcon
               alt="User"
               sx={{ width: "200px", height: "200px" }}
               className="flex bg-slate-200 w-48 rounded-full justify-center items-center"
-            ></Avatar>
+            ></AvatarIcon>
             <div className="flex flex-col grow justify-center">
               <div className="m-2 text-slate-200 text-lg">User Name</div>
               <div className="flex flex-row">
@@ -72,15 +35,15 @@ function page() {
               </div>
               <div className="flex gap-2 m-2">
                 <Button
-                  endIcon={<ArrowForward />}
+                  endIcon={<WhiteArrowForward />}
                   variant="contained"
                   sx={{
                     borderRadius: 28,
                     borderStyle: "solid",
                     borderColor: "white",
                     borderWidth: 2,
-                    bgcolor: "ochre.main",
-                    color: "primary.light",
+                    bgcolor: "#1a1a23",
+                    color: "#ffffff",
                   }}
                 >
                   Edit Profile 
@@ -92,8 +55,8 @@ function page() {
                     borderStyle: "solid",
                     borderColor: "white",
                     borderWidth: 2,
-                    bgcolor: "ochre.main",
-                    color: "primary.light",
+                    bgcolor: "#1a1a23",
+                    color: "#ffffff",
                   }}
                   endIcon={<Image src={LinkedInIcon} alt="123" />}
                 >
@@ -106,7 +69,7 @@ function page() {
           <div className="flex flex-col h-3/5 w-11/12 grow gap-6">
             <div className="flex rounded-3xl items-center justify-center relative z-0">
               <Image
-                src={imageSrc}
+                src={BargainBackgroundImage}
                 alt="Image Alt Text"
                 className="object-cover object-center"
                 style={{
@@ -126,8 +89,8 @@ function page() {
                       borderStyle: "solid",
                       borderColor: "white",
                       borderWidth: 2,
-                      bgcolor: "ochre.light",
-                      color: "primary.main",
+                      bgcolor: "#f6ff82",
+                      color: "#8e94e9",
                     }}
                   >
                     Share your discount
@@ -167,15 +130,15 @@ function page() {
               </div>
               <div className="flex grow items-center justify-center mt-6">
                 <Button
-                  endIcon={<ArrowForward />}
+                  endIcon={<WhiteArrowForward />}
                   variant="contained"
                   sx={{
                     borderRadius: 28,
                     borderStyle: "solid",
                     borderColor: "white",
                     borderWidth: 2,
-                    bgcolor: "ochre.main",
-                    color: "primary.light",
+                    bgcolor: "#1a1a23",
+                    color: "#ffffff",
                   }}
                 >
                   Share your discount
@@ -185,7 +148,6 @@ function page() {
           </div>
         </div>
       </div>
-    </ThemeProvider>
   );
 }
 
--- fwb/components/profile/WhiteArrowForward.tsx
+++ fwb/components/profile/WhiteArrowForward.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-const ArrowForward = (props: any) => (
+const WhiteArrowForward = (props: any) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={8} height={13} fill="none" {...props}>
     <path
       fill="#fff"
@@ -9,4 +9,4 @@ const ArrowForward = (props: any) => (
   </svg>
 );
 
-export default ArrowForward;
+export default WhiteArrowForward;
