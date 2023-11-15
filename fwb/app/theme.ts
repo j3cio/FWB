1eb39816f55a162078d46585225dd8487757399f
Derick Young <young.derickj@gmail.com>
2023-11-15 13:40:06 -0700

    Update theme
    Added all colors on the figma model to the theme

--- fwb/app/theme.ts
+++ fwb/app/theme.ts
@@ -1,22 +1,66 @@
 // theme.ts
 "use client";
 
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
+
+declare module "@mui/material/styles" {
+  interface Palette {
+    neutral: {
+      n900: string;
+      n800: string;
+      n700: string;
+      n600: string;
+      n500: string;
+      n400: string;
+      n300: string;
+      n200: string;
+      n100: string;
+    };
+  }
+  interface PaletteOptions {
+    neutral: {
+      n900: string;
+      n800: string;
+      n700: string;
+      n600: string;
+      n500: string;
+      n400: string;
+      n300: string;
+      n200: string;
+      n100: string;
+    };
+  }
+}
 
 const theme = createTheme({
- palette: {
-   primary: {
-     main: '#1a1a23',
-   },
-   secondary: {
-     main: '#f44336',
-   },
-   common: {
-    black: '#000',
-    white: '#fff',
-  }
- },
- // Add other theme configurations here
+  palette: {
+    primary: {
+      main: "#1A3E74",
+      light: "#24549E",
+      dark: "#11284B",
+    },
+    secondary: {
+      main: "#FBE093",
+      light: "#FDEEC4",
+      dark: "#F9D262",
+    },
+    neutral: {
+      n900: "#171A1F",
+      n800: "#2E343D",
+      n700: "#535D6F",
+      n600: "#68768D",
+      n500: "#8490A4",
+      n400: "#A1AAB9",
+      n300: "#BFC5CF",
+      n200: "#DCDFE5",
+      n100: "#F9FAFB",
+    },
+    common: {
+      black: "#000",
+      white: "#fff",
+    },
+  },
+  // Add other theme configurations here
 });
 
 export default theme;
