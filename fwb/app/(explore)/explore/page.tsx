d4b13390b148686149f3455650277542af031bfe
lai-kevin <kevin.lai.1@stonybrook.edu>
2023-11-22 01:00:59 -0500

    feat: connect clerk with supabase

--- fwb/app/(explore)/explore/page.tsx
+++ fwb/app/(explore)/explore/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import ResponsiveGrid from "@/components/ui/explore/products_grid";
 import { Container, Box, Typography } from "@mui/material";
 import Header from "@/components/ui/explore/header";
@@ -5,7 +6,32 @@ import AdSection from "@/components/ui/explore/ads_section";
 import MostPopular from "@/components/ui/explore/most_popular";
 import Productfilters from "@/components/ui/explore/productfilters";
 import { Divider } from "@mui/material";
+import { useEffect, useState } from "react";
+
+// Supabase + Clerk imports for accessing backend
+import { useAuth } from "@clerk/nextjs";
+import { getAllDiscounts } from "@/supabase/supabaseRequests";
 export default function ExplorePage() {
+  // Example of using accessing backend with supabase client
+  const { userId, getToken } = useAuth();
+  const [loadingDiscounts, setLoadingDiscounts] = useState(false);
+  const [discounts, setDiscounts] = useState<any[]>([]);
+  useEffect(() => {
+    const loadDiscounts = async () => {
+      let token = await getToken({ template: "supabase" });
+      token = token ? token : "";
+      let discounts = await getAllDiscounts( { userId, token});
+      discounts = discounts ? discounts : [];
+      setDiscounts(discounts);
+
+      console.log(discounts);
+    };
+    loadDiscounts();
+  }, [getToken, userId]);
+
+
+
+
   return (
     <Box sx={{ backgroundColor: "#1A1A23", minHeight: "100vh" }}>
       <Container disableGutters maxWidth="lg">
---
+++ fwb/supabase/supabaseRequests.ts
@@ -0,0 +1,12 @@
+import { supabaseClient } from "./supabaseClient";
+
+const getAllDiscounts = async ({ token } : { userId: string | undefined | null, token: string }) => {
+  const supabase = await supabaseClient(token);
+  const { data: discounts, error } = await supabase
+    .from("discounts")
+    .select("*");
+
+  return discounts;
+};
+
+export { getAllDiscounts };
