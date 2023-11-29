cd942fa7ffdee1262eee60bd2a44ee4e022348dd
Jeremy Seals <106201853+SealsJ@users.noreply.github.com>
2023-11-29 13:30:32 -0600

    Update layout.tsx

--- fwb/app/layout.tsx
+++ fwb/app/layout.tsx
@@ -15,6 +15,7 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return(
     <ThemeProvider theme={theme}>
       <ClerkProvider>
         <html lang="en">
