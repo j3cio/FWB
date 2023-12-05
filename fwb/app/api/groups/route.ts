3dfcb72c2856cba780e4ceba2c61187e784da2db
Derick Young <young.derickj@gmail.com>
2023-12-05 11:45:31 -0700

    Update GET, POST, PATCH AND DEL endpoints for groups

--- fwb/app/api/groups/route.ts
+++ fwb/app/api/groups/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { auth, currentUser } from "@clerk/nextjs";
 import supabaseClient from "@/supabase";
-
 interface Group {
   created_at: Date;
   name: string;
@@ -9,15 +8,15 @@ interface Group {
   public: boolean;
   discounts: string[]; // An array of discountIDs
   admins: string[]; // An array of userIDs who are admins within the group
-  // (have to work out how to give admin permissions within groups)
+  // have to work out how to give admin permissions within groups
 }
+
 // Get all groups
 export async function GET(request: NextRequest & { query: Record<string, string> }, response: NextResponse) {
   try {
     // Fetch all public groups
     const supabase = await supabaseClient();
-    let { data, error } = await supabase.from("groups").select("name");
-
+    let { data, error } = await supabase.from("groups").select("*");
     if (error) {
       return NextResponse.json({ error: "Failed to fetch groups" }, { status: 500 });
     }
@@ -36,12 +35,11 @@ export async function POST(request: NextRequest, response: NextResponse) {
     if (userId && user) {
       const formData = await request.formData();
       const newGroup = {
-        created_at: new Date(),
         name: formData.get("name"),
         users: formData.get("users"), // Array in the form
-        public: formData.get("public"),
         discounts: formData.get("discounts"), // Array in the form
-        admins: formData.get("admin"),
+        admins: formData.get("admins"),
+        public: formData.get("public"),
       };
 
       // Create a Supabase client with the current user's access token
@@ -53,6 +51,7 @@ export async function POST(request: NextRequest, response: NextResponse) {
 
       // Insert the new discount into the database
       const { data, error } = await supabase.from("groups").insert([newGroup]).select();
+
       if (error) {
         return NextResponse.json({ error: "Failed to create group" }, { status: 500 });
       }
@@ -68,19 +67,18 @@ export async function POST(request: NextRequest, response: NextResponse) {
 // Delete a group
 export async function DELETE(request: NextRequest, response: NextResponse) {
   try {
-    const { userId, getToken } = auth();
+    const { userId } = auth();
     const user = await currentUser();
 
     if (userId && user) {
       const group_id = request.nextUrl.searchParams.get("group_id");
 
       // Create a Supabase client with the current user's access token
-      const token = await getToken({ template: "supabase" });
+      const token = request.headers.get("supabase_jwt");
       if (!token) {
-        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+        return NextResponse.json({ error: "Could not create supabase access token" }, { status: 401 });
       }
       const supabase = await supabaseClient(token);
-
       const { error } = await supabase.from("groups").delete().eq("id", group_id);
 
       if (error) {
@@ -92,6 +90,51 @@ export async function DELETE(request: NextRequest, response: NextResponse) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
   } catch (error) {
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
+// Update a group
+export async function PATCH(request: NextRequest, response: NextResponse) {
+  try {
+    const { userId } = auth();
+    const user = await currentUser();
+
+    if (userId && user) {
+      //const group_id = request.nextUrl.searchParams.get("group_id");
+      const formData = await request.formData();
+
+      const updatedGroup = {
+        name: formData.get("name"),
+        users: formData.get("users"), // Array
+        discounts: formData.get("discounts"), // Array
+        admins: formData.get("admins"), // Array
+        public: formData.get("public"),
+      };
+
+      // Create a Supabase client with the current user's access token
+      const token = request.headers.get("supabase_jwt");
+
+      if (!token) {
+        return NextResponse.json({ error: "Could not create supabase access token" }, { status: 401 });
+      }
+      const supabase = await supabaseClient(token);
+
+      // Update the group in the database
+      const { data, error } = await supabase
+        .from("groups")
+        .update(updatedGroup)
+        .eq("id", "603f958e-8760-4f3a-8ecc-ce979e2433dc") // TODO: This uuid should not be hardcoded
+        .select();
+      console.log(error);
+      if (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+      }
+      return NextResponse.json({ success: true, data }, { status: 200 });
+    } else {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+  } catch (error) {
+    console.log(error);
     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
