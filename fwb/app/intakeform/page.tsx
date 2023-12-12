4b15fbd1dd17e55592e55a08f6bcca4ffecf7251
kkj2010 <kunjukim0210@gmail.com>
2023-12-11 20:19:10 -0500

    fix select options

--- fwb/app/intakeform/page.tsx
+++ fwb/app/intakeform/page.tsx
@@ -14,6 +14,7 @@ import Select, { SelectChangeEvent } from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import InputLabel from "@mui/material/InputLabel";
 import FormControl from "@mui/material/FormControl";
+import Theme from "../theme";
 
 const theme = createTheme({
   components: {
@@ -33,7 +34,7 @@ const theme = createTheme({
         //   backgroundColor: 'red',
         // },
         rail: {
-          backgroundColor: "red",
+          backgroundColor: "white !important",
         },
         valueLabel: {
           backgroundColor: "#FFF", // Label background color
@@ -48,8 +49,13 @@ const theme = createTheme({
 });
 
 export default function Intakeform() {
-  // const theme = useTheme();
   const [discount, setDiscount] = useState(0);
+  const [discountAmount, setDiscountAmount] = useState("");
+  const [selectedOption, setSelectedOption] = useState<"public" | "private">(
+    "public"
+  );
+  const [categories, setCategories] = useState([]);
+
   const handleSlide = (event: Event, newValue: number | number[]) => {
     if (typeof newValue === "number") {
       setDiscount(newValue);
@@ -64,10 +70,16 @@ export default function Intakeform() {
     return `${discount}%`;
   };
 
-  const [selectedOption, setSelectedOption] = useState("");
+  const handleOptionChange = (option: "public" | "private") => {
+    setSelectedOption(option);
+  };
 
-  const handleUpdate = (event: SelectChangeEvent) => {
-    setSelectedOption(event.target?.value);
+  // const handleUpdate = (event: SelectChangeEvent) => {
+  //   setSelectedOption(event.target?.value);
+  // };
+
+  const handleCategoryChange = (selectedCategories: any) => {
+    setCategories(selectedCategories);
   };
 
   return (
@@ -178,14 +190,13 @@ export default function Intakeform() {
                             <Slider
                               sx={{
                                 "& .MuiSlider-rail": {
-                                  backgroundColor: "white",
+                                  backgroundColor: "white !important",
                                 },
                               }}
                               value={discount}
                               onChange={handleSlide}
                               valueLabelDisplay="auto"
                               valueLabelFormat={valueLabelFormat}
-                              // valueLabelStyle={{color:'#1A1A23', backgroundColor:'#FFF'}}
                               aria-labelledby="continuous-slider"
                             />
                           </div>
@@ -232,16 +243,80 @@ export default function Intakeform() {
                     </div>
                     <div className="categories">
                       <div className="category">Category*</div>
-                      <div className="select">
+                      {/* <div className="flex justify-start"> */}
+                      {/* <div className="selectCategory"> */}
+                        <select
+                          onChange={(e) =>
+                            handleCategoryChange(
+                              Array.from(
+                                e.target.selectedOptions,
+                                (option) => option.value
+                              )
+                            )
+                          }
+                          value={categories}
+                          required
+                        >
+                          <option value="All">All </option>
+                          <option value="Sports">Sports</option>
+                          <option value="Fashion">Fashion</option>
+                          <option value="Electronic">Electronic</option>
+                          <option value="Health">Health</option>
+                          <option value="HomeAndKitchen">Home & Kitchen</option>
+                          <option value="ComputerAndAccessories">
+                            Computer & Accessories
+                          </option>
+                          <option value="BeautyAndSkincare">
+                            Beauty & Skincare
+                          </option>
+                          <option value="Books">Books</option>
+                          <option value="Hobbies">Hobbies</option>
+                        </select>
+                      {/* </div> */}
+                      {/* </div> */}
+                      {/* <div className="select">
                         <FormControl
                           fullWidth
                           sx={{
-                            width: "386px",
-                            height: "48px",
+                            width: "354px",
+                            height: "16px",
                             borderRadius: "10px",
                           }}
                         >
-                          <InputLabel id="select-label">All</InputLabel>
+                          <InputLabel
+                            id="select-label"
+                            sx={{
+                              color: "white",
+                              paddingLeft: "5px",
+                              marginTop: "-5px",
+                              borderRadius: "10px",
+                            }}
+                          >
+                            <div className="all">
+                              All
+                              <div className="arrowImage">
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  width="24"
+                                  height="24"
+                                  viewBox="0 0 24 24"
+                                  fill="none"
+                                  style={
+                                    {
+                                      // order: 0,
+                                      // zIndex: 1,
+                                      // position: "absolute",
+                                    }
+                                  }
+                                >
+                                  <path
+                                    d="M15.8746 9.00001L11.9946 12.88L8.11461 9.00001C7.92778 8.81275 7.67413 8.70752 7.40961 8.70752C7.14509 8.70752 6.89144 8.81275 6.70461 9.00001C6.31461 9.39001 6.31461 10.02 6.70461 10.41L11.2946 15C11.6846 15.39 12.3146 15.39 12.7046 15L17.2946 10.41C17.6846 10.02 17.6846 9.39001 17.2946 9.00001C16.9046 8.62001 16.2646 8.61001 15.8746 9.00001Z"
+                                    fill="white"
+                                  />
+                                </svg>
+                              </div>
+                            </div>
+                          </InputLabel>
                           <Select
                             labelId="select-label"
                             id="select"
@@ -249,22 +324,17 @@ export default function Intakeform() {
                             label="Select Option"
                             onChange={handleUpdate}
                             sx={{
-                              background: "white",
+                              background: "black",
+                              borderColor: "#8e94e9",
                               color: "white",
-                              "&:before": {
-                                borderColor: "#8e94e9",
-                              },
-                              "&:after": {
-                                borderColor: "#8e94e9",
-                              },
-                              "& .MuiSelect-icon": {
-                                color: "white",
-                              },
+                              borderWidth: "3px",
+                              width: "386px",
+                              height: "48px",
+                              borderRadius: "10px",
+                              
                             }}
                           >
-                            {/* <MenuItem value="">
-          <em>None</em>
-        </MenuItem> */}
+                           
                             <MenuItem value="option1">All</MenuItem>
                             <MenuItem value="option2">Sports</MenuItem>
                             <MenuItem value="option3">Fashion</MenuItem>
@@ -280,104 +350,114 @@ export default function Intakeform() {
                             <MenuItem value="option9">Books</MenuItem>
                             <MenuItem value="option10">Hobbies</MenuItem>
                           </Select>
-                        </FormControl>
+                        </FormControl> */}
+                    </div>
+                  </div>
+                  <div className="rule">
+                    <div className="discountRule">
+                      Discount Rules* <p>& Conditions</p>
+                    </div>
+                    <div>
+                      {" "}
+                      <input
+                        className="inputDiscount"
+                        placeholder=""
+                        // onChange={(e) => setEmailAddress(e.target.value)}
+                        id="discountRule"
+                        name="discountRule"
+                        type="discountRule"
+                      />
+                    </div>
+                  </div>
+                  <div className="share">
+                    <label className="shareOn">Share on*</label>
+
+                    <div
+                      onClick={() => handleOptionChange("public")}
+                      className={`public ${
+                        selectedOption === "public" ? "selectClass" : ""
+                      }`}
+                    >
+                      <span className="pic">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="40"
+                          height="40"
+                          viewBox="0 0 40 40"
+                          fill="none"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            clip-rule="evenodd"
+                            d="M19.9987 3.33301C10.7987 3.33301 3.33203 10.7997 3.33203 19.9997C3.33203 29.1997 10.7987 36.6663 19.9987 36.6663C29.1987 36.6663 36.6654 29.1997 36.6654 19.9997C36.6654 10.7997 29.1987 3.33301 19.9987 3.33301ZM18.332 33.2163C11.7487 32.3996 6.66536 26.7996 6.66536 19.9996C6.66536 18.9663 6.7987 17.9829 7.01536 17.0163L14.9987 24.9996V26.6663C14.9987 28.4996 16.4987 29.9996 18.332 29.9996V33.2163ZM26.6654 26.6661C28.1654 26.6661 29.3987 27.6327 29.832 28.9827C31.9987 26.6161 33.332 23.4661 33.332 19.9994C33.332 14.4161 29.882 9.63275 24.9987 7.64941V8.33275C24.9987 10.1661 23.4987 11.6661 21.6654 11.6661H18.332V14.9994C18.332 15.9161 17.582 16.6661 16.6654 16.6661H13.332V19.9994H23.332C24.2487 19.9994 24.9987 20.7494 24.9987 21.6661V26.6661H26.6654Z"
+                            fill="white"
+                          />
+                        </svg>
+                      </span>
+
+                      <div className="publicLeft">
+                        <span>Public</span>
+                        <span>Lorem ipsum dolor sit amet consectetur</span>
                       </div>
                     </div>
-                    <div className="rule">
-                      <div className="discountRule">
-                        Discount Rules* <p>& Conditions</p>
+
+                    <div
+                      onClick={() => handleOptionChange("private")}
+                      className={`private ${
+                        selectedOption === "private" ? "selectPrivate" : ""
+                      }`}
+                    >
+                      <span className="pic">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="40"
+                          height="40"
+                          viewBox="0 0 40 40"
+                          fill="none"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            clip-rule="evenodd"
+                            d="M28.3346 14.1667H30.0013C31.8346 14.1667 33.3346 15.6667 33.3346 17.5V34.1667C33.3346 36 31.8346 37.5 30.0013 37.5H10.0013C8.16797 37.5 6.66797 36 6.66797 34.1667V17.5C6.66797 15.6667 8.16797 14.1667 10.0013 14.1667H11.668V10.8333C11.668 6.23333 15.4013 2.5 20.0013 2.5C24.6013 2.5 28.3346 6.23333 28.3346 10.8333V14.1667ZM20.0013 5.83333C17.2346 5.83333 15.0013 8.06667 15.0013 10.8333V14.1667H25.0013V10.8333C25.0013 8.06667 22.768 5.83333 20.0013 5.83333ZM11.668 34.1667C10.7513 34.1667 10.0013 33.4167 10.0013 32.5V19.1667C10.0013 18.25 10.7513 17.5 11.668 17.5H28.3346C29.2513 17.5 30.0013 18.25 30.0013 19.1667V32.5C30.0013 33.4167 29.2513 34.1667 28.3346 34.1667H11.668ZM23.3346 25.8333C23.3346 27.6667 21.8346 29.1667 20.0013 29.1667C18.168 29.1667 16.668 27.6667 16.668 25.8333C16.668 24 18.168 22.5 20.0013 22.5C21.8346 22.5 23.3346 24 23.3346 25.8333Z"
+                            fill="white"
+                          />
+                        </svg>
+                      </span>
+
+                      <div className="publicLeft">
+                        <span>Private Group</span>
+                        <span>Lorem ipsum dolor sit amet consectetur</span>
                       </div>
-                      <div>
-                        {" "}
-                        <input
-                          className="inputDiscount"
-                          placeholder=""
-                          // onChange={(e) => setEmailAddress(e.target.value)}
-                          id="discountRule"
-                          name="discountRule"
-                          type="discountRule"
-                        />
-                      </div>
-                    </div>
-                    <div className="share">
-                      <div className="shareOn">Share on*</div>
-                      <a className="public">
-                        <span className="pic">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="40"
-                            height="40"
-                            viewBox="0 0 40 40"
-                            fill="none"
-                          >
-                            <path
-                              fill-rule="evenodd"
-                              clip-rule="evenodd"
-                              d="M19.9987 3.33301C10.7987 3.33301 3.33203 10.7997 3.33203 19.9997C3.33203 29.1997 10.7987 36.6663 19.9987 36.6663C29.1987 36.6663 36.6654 29.1997 36.6654 19.9997C36.6654 10.7997 29.1987 3.33301 19.9987 3.33301ZM18.332 33.2163C11.7487 32.3996 6.66536 26.7996 6.66536 19.9996C6.66536 18.9663 6.7987 17.9829 7.01536 17.0163L14.9987 24.9996V26.6663C14.9987 28.4996 16.4987 29.9996 18.332 29.9996V33.2163ZM26.6654 26.6661C28.1654 26.6661 29.3987 27.6327 29.832 28.9827C31.9987 26.6161 33.332 23.4661 33.332 19.9994C33.332 14.4161 29.882 9.63275 24.9987 7.64941V8.33275C24.9987 10.1661 23.4987 11.6661 21.6654 11.6661H18.332V14.9994C18.332 15.9161 17.582 16.6661 16.6654 16.6661H13.332V19.9994H23.332C24.2487 19.9994 24.9987 20.7494 24.9987 21.6661V26.6661H26.6654Z"
-                              fill="white"
-                            />
-                          </svg>
-                        </span>
-
-                        <div className="publicLeft">
-                          <span>Public</span>
-                          <span>Lorem ipsum dolor sit amet consectetur</span>
-                        </div>
-                      </a>
-
-                      <a className="private">
-                        <span className="pic">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="40"
-                            height="40"
-                            viewBox="0 0 40 40"
-                            fill="none"
-                          >
-                            <path
-                              fill-rule="evenodd"
-                              clip-rule="evenodd"
-                              d="M28.3346 14.1667H30.0013C31.8346 14.1667 33.3346 15.6667 33.3346 17.5V34.1667C33.3346 36 31.8346 37.5 30.0013 37.5H10.0013C8.16797 37.5 6.66797 36 6.66797 34.1667V17.5C6.66797 15.6667 8.16797 14.1667 10.0013 14.1667H11.668V10.8333C11.668 6.23333 15.4013 2.5 20.0013 2.5C24.6013 2.5 28.3346 6.23333 28.3346 10.8333V14.1667ZM20.0013 5.83333C17.2346 5.83333 15.0013 8.06667 15.0013 10.8333V14.1667H25.0013V10.8333C25.0013 8.06667 22.768 5.83333 20.0013 5.83333ZM11.668 34.1667C10.7513 34.1667 10.0013 33.4167 10.0013 32.5V19.1667C10.0013 18.25 10.7513 17.5 11.668 17.5H28.3346C29.2513 17.5 30.0013 18.25 30.0013 19.1667V32.5C30.0013 33.4167 29.2513 34.1667 28.3346 34.1667H11.668ZM23.3346 25.8333C23.3346 27.6667 21.8346 29.1667 20.0013 29.1667C18.168 29.1667 16.668 27.6667 16.668 25.8333C16.668 24 18.168 22.5 20.0013 22.5C21.8346 22.5 23.3346 24 23.3346 25.8333Z"
-                              fill="white"
-                            />
-                          </svg>
-                        </span>
-
-                        <div className="publicLeft">
-                          <span>Private Group</span>
-                          <span>Lorem ipsum dolor sit amet consectetur</span>
-                        </div>
-                      </a>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div>
-              <div className="agree">
-                <span>
-                  <FormControlLabel
-                    control={
-                      <Checkbox
-                        checked={checked}
-                        onChange={handleChange}
-                        style={{ color: "white" }} // You can customize the color here
-                      />
-                    }
-                    label=""
+          </div>
+        </div>
+        <div className="submit">
+          <div className="agree">
+            <span>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={checked}
+                    onChange={handleChange}
+                    style={{ color: "white" }} // You can customize the color here
                   />
-                </span>
-                <span className="termAgree">I agree to the</span>{" "}
-                <a className="terms">Terms & Privacy Policy</a>
-              </div>
-              <div className="submitButtons">
-                <div>
-                  <button className="save">Save and Share</button>
-                </div>
-                <div>
-                  <button className="cancel">Cancel</button>
-                </div>
-              </div>
+                }
+                label=""
+              />
+            </span>
+            <span className="termAgree">I agree to the</span>{" "}
+            <a className="terms">Terms & Privacy Policy</a>
+          </div>
+          <div className="submitButtons">
+            <div>
+              <button className="save">Save and Share</button>
+            </div>
+            <div>
+              <button className="cancel">Cancel</button>
             </div>
           </div>
         </div>
