001e4c80a7d3081f8b20aef3a1bfc074c22f094a
Derick Young <young.derickj@gmail.com>
2023-12-12 21:12:53 -0700

    Create bargains picture component

---
+++ fwb/components/ui/privategroups/bargains_picture.jsx
@@ -0,0 +1,43 @@
+import theme from "@/app/theme";
+import BlueArrowForward from "@/components/profile/BlueArrowForward";
+import { Button } from "@mui/material";
+import Image from "next/image";
+import BargainBackgroundImage from "../../../public/bargain1700x350.png";
+
+const bargains_picture = () => {
+  return (
+    <div className="rounded-3xl items-center justify-center relative z-0">
+      <Image
+        src={BargainBackgroundImage}
+        alt="Image Alt Text"
+        className="object-cover object-center"
+        style={{
+          width: "100%",
+          height: "auto",
+          objectFit: "cover",
+        }}
+      />
+      <div className="absolute inset-0 flex items-center justify-center">
+        <h1 className="flex w-1/6 ml-48 mt-10 text-6xl font-bold text-yellow-200">Booty Call for Bargains!</h1>
+        <div className="flex flex-row-reverse w-5/6 mr-28">
+          <Button
+            endIcon={<BlueArrowForward />}
+            variant="contained"
+            sx={{
+              borderRadius: 28,
+              borderStyle: "solid",
+              borderColor: "white",
+              borderWidth: 2,
+              bgcolor: `${theme.palette.secondary.main}`,
+              color: `${theme.palette.primary.light}`,
+            }}
+          >
+            Share your discount
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default bargains_picture;
