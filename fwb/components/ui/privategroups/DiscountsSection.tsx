bd0e394d15383cce31dda77404f7b93d654ceee6
Derick Young <young.derickj@gmail.com>
2023-12-14 12:50:55 -0700

    Create Discounts Section

---
+++ fwb/components/ui/privategroups/DiscountsSection.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Productfilters from "@/components/ui/explore/productfilters";
+import ResponsiveGrid from "@/components/ui/explore/products_grid";
+
+const DiscountsSection = () => {
+  return (
+    <div>
+        <Productfilters />
+        <ResponsiveGrid />
+    </div>
+  )
+}
+
+export default DiscountsSection
