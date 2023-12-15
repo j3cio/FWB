231be52c12f61fff285e2c6416462a1174ff89f8
Derick Young <young.derickj@gmail.com>
2023-12-14 20:10:31 -0700

    Create Tabs

--- fwb/app/(privategroup)/privategroup/page.tsx
+++ fwb/app/(privategroup)/privategroup/page.tsx
@@ -1,24 +1,17 @@
 "use client";
 import Header from "@/components/ui/explore/header";
 import Bargains from "../../../components/ui/privategroups/bargains_picture";
-import DiscountsSection from "@/components/ui/privategroups/DiscountsSection";
-import MembersSection from "@/components/ui/privategroups/MembersSection";
-import { useState } from "react";
-
-
-
+import Tabs from "@/components/ui/privategroups/Tabs";
 
 const page = () => {
-
-  const [showMembers, useShowMembers] = useState(false)
+  
 
   return (
     <div className="bg-[#1a1a23] min-h-screen">
       <Header />
       <div> Profile Section </div>
       <Bargains />
-      <div> Tab Section </div>
-      {showMembers ? <MembersSection /> : <DiscountsSection />}
+      <Tabs />
     </div>
   );
 };
