530e522cea26139542a95e05d775fec209f679c8
Kevin Lai <kevinlaisoftware@gmail.com>
2023-12-23 19:13:47 -0500

    refactor patch handler

--- fwb/app/api/discounts/route.ts
+++ fwb/app/api/discounts/route.ts
@@ -208,7 +208,13 @@ export async function GET(request: NextRequest) {
   }
 }
 
-// Delete a discount
+/**
+ * Deletes a discount based on the provided discount ID.
+ *
+ * @param request - The NextRequest object representing the incoming request.
+ * @param response - The NextResponse object representing the outgoing response.
+ * @returns A JSON response indicating the success or failure of the delete operation.
+ */
 export async function DELETE(request: NextRequest, response: NextResponse) {
   const { userId } = auth();
 
@@ -254,10 +260,89 @@ export async function DELETE(request: NextRequest, response: NextResponse) {
       { status: 500 }
     );
   }
-  return NextResponse.json({ success: true, deleted: discount_id }, { status: 200 });
+  return NextResponse.json(
+    { success: true, deleted: discount_id },
+    { status: 200 }
+  );
 }
 
 // Update a discount
 export async function PATCH(request: NextRequest, response: NextResponse) {
-  return await updateDiscount(request);
+  const { userId } = auth();
+
+  // Check if the user is logged in
+  if (!userId) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+  const user = await currentUser();
+  if (!user) {
+    return NextResponse.json(
+      { error: "Unauthorized. Failed to obtain current User" },
+      { status: 401 }
+    );
+  }
+
+  // Create a Supabase client with the current user's access token
+  const token = request.headers.get("supabase_jwt");
+  if (!token) {
+    return NextResponse.json(
+      { error: "Missing supabase_jwt token" },
+      { status: 401 }
+    );
+  }
+  // DEV NOTE: This is a temporary fix to get around the fact that we can't create a supabase_jwt with a long-lived session token
+  const supabase = await supabaseClient(token);
+  if (!supabase) {
+    return NextResponse.json(
+      { error: "Failed to create supabase client" },
+      { status: 401 }
+    );
+  }
+
+  // Extract the form data
+  const formData = await request.formData();
+  const updatedDiscount: any = {};
+
+  const discount_id = formData.get("discount_id");
+
+  if (formData.get("company")) {
+    updatedDiscount.company = formData.get("company");
+  }
+  if (formData.get("terms_and_conditions")) {
+    updatedDiscount.terms_and_conditions = formData.get("terms_and_conditions");
+  }
+  if (formData.get("company_url")) {
+    updatedDiscount.company_url = formData.get("company_url");
+  }
+  if (formData.get("discount_amount")) {
+    updatedDiscount.discount_amount = Number(formData.get("discount_amount"));
+  }
+  if (formData.get("public")) {
+    updatedDiscount.public = formData.get("public") === "true" ? true : false;
+  }
+  if (formData.get("private_groups")) {
+    updatedDiscount.private_groups = String(
+      formData.get("private_groups")
+    ).split(",");
+  }
+
+  // Update the discount in the database
+  const { data, error } = await supabase
+    .from("discounts")
+    .update(updatedDiscount)
+    .eq("id", discount_id)
+    .select();
+
+  if (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "Failed to update discount" },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json(
+    { success: true, updated_values: data },
+    { status: 200 }
+  );
 }
