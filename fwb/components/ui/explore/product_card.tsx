f719ff42dbff4b3dbeeba13a2e988f84c979f09b
lai-kevin <kevin.lai.1@stonybrook.edu>
2023-12-26 09:40:24 -0500

    change discount circle to greatest discount

--- fwb/components/ui/explore/product_card.tsx
+++ fwb/components/ui/explore/product_card.tsx
@@ -9,89 +9,81 @@ import Avatar from "@mui/material/Avatar";
 import { CardActionArea } from "@mui/material";
 import { motion } from "framer-motion";
 
-
-/**
- * Renders a circle component with animation based on hover state.
- * @param {boolean} isHovered - Indicates whether the circle is being hovered.
- * @returns {JSX.Element} The rendered circle component.
- */
-const Circle = ({isHovered} : {isHovered: boolean}) => {
-  return (
-    <div style={{ position: "relative", fontFamily: "inherit" }}>
-      <motion.div
-        animate={{ y: isHovered ? -10 : 0 }} // Move up 10 pixels when hovering
-        style={{
-          position: "absolute",
-          top: -55,
-          left: 180,
-          width: "60px",
-          height: "60px",
-          borderRadius: "50%",
-          backgroundColor: "#8e94e9",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          fontFamily: "inherit"
-        }}
-      >
-        <Box sx={{ justifyContent: "center", alignItems: "center", fontFamily: "inherit" }}>
-          <Typography
-            sx={{
-              color: "#F6FF82",
-              fontSize: "24px",
-              fontWeight: "600",
-              lineHeight: "20px",
-              fontFamily: "inherit",
-              fontStyle: "normal",
-            }}
-          >
-            50%
-          </Typography>
-          <Typography
-            sx={{
-              color: "#F6FF82",
-              fontSize: "12px",
-              fontWeight: "400",
-              textAlign: "right",
-              fontFamily: "inherit",
-              fontStyle: "normal",
-              lineHeight: "14px",
-            }}
-          >
-            off
-          </Typography>
-        </Box>
-      </motion.div>
-    </div>
-  );
-};
-
-
 /**
  * Renders a discount component.
  * @param {boolean} isHovered - Indicates whether the component is being hovered.
- * @returns {JSX.Element} The rendered discount component.
+ * @param {number} amount - The discount amount in percentage.
+ * @returns {JSX.Element} The discount component.
  */
-const Discount = ({isHovered}: {isHovered: boolean}) => {
+const Discount = ({ isHovered, amount }: { isHovered: boolean, amount: Number }) => {
   return (
     <Box>
-      <Circle isHovered={isHovered} />
+      <div style={{ position: "relative", fontFamily: "inherit" }}>
+        <motion.div
+          animate={{ y: isHovered ? -10 : 0 }} // Move up 10 pixels when hovering
+          style={{
+            position: "absolute",
+            top: -55,
+            left: 180,
+            width: "60px",
+            height: "60px",
+            borderRadius: "50%",
+            backgroundColor: "#8e94e9",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontFamily: "inherit",
+          }}
+        >
+          <Box
+            sx={{
+              justifyContent: "center",
+              alignItems: "center",
+              fontFamily: "inherit",
+            }}
+          >
+            <Typography
+              sx={{
+                color: "#F6FF82",
+                fontSize: "24px",
+                fontWeight: "600",
+                lineHeight: "20px",
+                fontFamily: "inherit",
+                fontStyle: "normal",
+              }}
+            >
+              {`${amount}%`}
+            </Typography>
+            <Typography
+              sx={{
+                color: "#F6FF82",
+                fontSize: "12px",
+                fontWeight: "400",
+                textAlign: "right",
+                fontFamily: "inherit",
+                fontStyle: "normal",
+                lineHeight: "14px",
+              }}
+            >
+              off
+            </Typography>
+          </Box>
+        </motion.div>
+      </div>
     </Box>
   );
 };
-
-
 
 /**
  * Renders a product card component.
  * @returns JSX.Element
  */
-export default function ProductCard({company}: {company: any}) {
+export default function ProductCard({ company }: { company: any }) {
   const [isHovered, setIsHovered] = React.useState(false); // Indicates whether the card is being hovered
   return (
     <motion.div
-    onHoverStart={() => setIsHovered(true)}
-    onHoverEnd={() => setIsHovered(false)}
+      onHoverStart={() => setIsHovered(true)}
+      onHoverEnd={() => setIsHovered(false)}
     >
       {/* Card Component */}
       <Box
@@ -116,7 +108,12 @@ export default function ProductCard({company}: {company: any}) {
             component="img"
             image={`${company.logo}`}
             alt={`${company.name} logo`}
-            sx={{ height: "72%", padding: "0px", borderTopLeftRadius: "20px", borderTopRightRadius: "20px" }}
+            sx={{
+              height: "72%",
+              padding: "0px",
+              borderTopLeftRadius: "20px",
+              borderTopRightRadius: "20px",
+            }}
           />
           {/* Card Content */}
           <CardContent
@@ -131,47 +128,74 @@ export default function ProductCard({company}: {company: any}) {
           >
             <Box sx={{ display: "flex", flexDirection: "row" }}>
               <Typography
-                sx={{ fontSize: 24, 
+                sx={{
+                  fontSize: 24,
                   fontWeight: "600",
                   wordWrap: "break-word",
                   fontFamily: "inherit",
                   fontStyle: "normal",
                   lineHeight: "26.4px",
-                 }}
+                }}
               >
                 {company.name}
               </Typography>
             </Box>
-            <Discount isHovered={isHovered}/>
+            <Discount isHovered={isHovered} amount={company.greatest_discount} />
 
             {/*Profile Pictures of Users Offering Discounts for The Company*/}
-            <Box sx={{ display: "flex", flexDirection: "row", marginY: "4px", alignItems: "center" }}>
-              <div style={{ position: "relative", width: "64px", height: "24px" }}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                marginY: "4px",
+                alignItems: "center",
+              }}
+            >
+              <div
+                style={{ position: "relative", width: "64px", height: "24px" }}
+              >
                 <Avatar
                   alt="man1"
                   src="https://www.svgrepo.com/show/303108/google-icon-logo.svg"
-                  sx={{ width: "24px", height: "24px", position: "absolute", left: "0" }}
+                  sx={{
+                    width: "24px",
+                    height: "24px",
+                    position: "absolute",
+                    left: "0",
+                  }}
                 />
                 <Avatar
                   alt="man1"
                   src="https://www.svgrepo.com/show/303108/google-icon-logo.svg"
-                  sx={{ width: "24px", height: "24px", position: "absolute", left: "20px" }}
+                  sx={{
+                    width: "24px",
+                    height: "24px",
+                    position: "absolute",
+                    left: "20px",
+                  }}
                 />
                 <Avatar
                   alt="man1"
                   src="https://www.svgrepo.com/show/303108/google-icon-logo.svg"
-                  sx={{ width: "24px", height: "24px", position: "absolute", left: "40px" }}
+                  sx={{
+                    width: "24px",
+                    height: "24px",
+                    position: "absolute",
+                    left: "40px",
+                  }}
                 />
               </div>
-              <Typography sx={{
-                fontSize: "12px",
-                fontWeight: "400",
-                fontFamily: "inherit",
-                fontStyle: "normal",
-                color: "#6B77AD",
-                lineHeight: "18px",
-                marginLeft: "6px",
-              }}>
+              <Typography
+                sx={{
+                  fontSize: "12px",
+                  fontWeight: "400",
+                  fontFamily: "inherit",
+                  fontStyle: "normal",
+                  color: "#6B77AD",
+                  lineHeight: "18px",
+                  marginLeft: "6px",
+                }}
+              >
                 +{company.discounts.length} Benefits available
               </Typography>
             </Box>
