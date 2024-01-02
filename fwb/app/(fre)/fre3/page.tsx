a23a1eed771ada412bc8d786464f4eed73f1ab3a
Derick Young <young.derickj@gmail.com>
2024-01-02 13:48:29 -0700

    Add Facebook sharing button

--- fwb/app/(fre)/fre3/page.tsx
+++ fwb/app/(fre)/fre3/page.tsx
@@ -1,5 +1,7 @@
 "use client";
-
+import {
+  FacebookShareButton,
+} from "react-share";
 import "./page.css";
 import Link from "next/link";
 import { useUser } from "@clerk/nextjs";
@@ -126,7 +128,9 @@ export default function UserFlowPage3() {
             <button className="icon1" onClick={handleinstagram}>
               <img src="/socialicons/instagram.SVG" />
             </button>
-            <button className="icon1" onClick={handlefacebook}>
+            <div className="icon1">
+            {/* The url given is the url link you would like to share */}
+            <FacebookShareButton url={"app.makefwb.com"}>
               <div className="facebookIcon">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -142,7 +146,8 @@ export default function UserFlowPage3() {
                 </svg>
               </div>
               {/* <img src="/socialicons/facebook.SVG" /> */}
-            </button>
+              </FacebookShareButton>
+              </div>
             <button onClick={handlediscord}>
               <img src="/socialicons/discord.SVG" />
             </button>
