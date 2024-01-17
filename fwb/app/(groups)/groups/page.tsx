82a2ec5a69b2b33ce852de38a762fc4ac29884b6
Derick Young <young.derickj@gmail.com>
2024-01-16 17:05:16 -0700

    Display group data in group details section

--- fwb/app/(groups)/groups/page.tsx
+++ fwb/app/(groups)/groups/page.tsx
@@ -74,7 +74,7 @@ const page = async ({ searchParams }: { searchParams: { [key: string]: string | 
   return (
     <div className="bg-[#1a1a23] h-screen w-screen overflow-x-hidden">
       <Header />
-      <GroupDetailsSection />
+      <GroupDetailsSection userData={userData} groupData={groupData.data[0]}/>
       <Tabs userData={userData} groupData={groupData.data[0]} />
     </div>
   );
