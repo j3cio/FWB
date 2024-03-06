f7620ef6600e0e2fb0b736fcb3b8cd84de1a0d7b
Brandon Lee <brandonlee2543@hotmail.com>
2024-03-06 18:47:11 -0400

    Added loading Skeleton to our DIscountsSection

--- fwb/components/ui/privategroups/groupdetailspage/DiscountsSection.tsx
+++ fwb/components/ui/privategroups/groupdetailspage/DiscountsSection.tsx
@@ -2,21 +2,33 @@ import { DiscountData } from "@/app/types/types";
 import Productfilters from "@/components/ui/explore/productfilters";
 import { Box, Grid } from "@mui/material";
 import DiscountCard from "./DiscountCard";
+import { generateSkeletons } from "../../skeletons/generateSkeletons";
 
-const DiscountsSection = ({ discountData }: { discountData: DiscountData[] }) => {
+const DiscountsSection = ({ discountData, isLoading }: { discountData: DiscountData[], isLoading: boolean }) => {
   return (
     <div className="bg-[#1a1a23]">
       <div className="w-11/12 bg-white">
         <Productfilters />
       </div>
-      <div className=" flex justify-center ml-24">
+      <div className="flex justify-center">
         <Box sx={{ flexGrow: 1, paddingBottom: "20px", justifyContent: "center", minHeight: "1706px" }}>
           <Grid container spacing={2} rowGap={2} sx={{ marginBottom: "60px" }}>
-            {discountData.map((company: any, index: React.Key) => (
-              <Grid item xs={12} sm={6} md={3} key={index} sx={{ width: "282px", height: "322px" }}>
+              {isLoading ? (
+                <div className="flex flex-wrap gap-x-4 ml-2">{generateSkeletons({ type: "ProductCard", quantity: 20 })}</div>
+              ) : (
+                discountData.map((company: any, index: React.Key) => (
+                <Grid
+                  item
+                  xs={12}
+                  sm={6}
+                  md={3}
+                  key={index}
+                  sx={{ width: "282px", height: "322px" }}
+               >
                 <DiscountCard company={company} />
-              </Grid>
-            ))}
+               </Grid>
+            ))
+          )}
           </Grid>
         </Box>
       </div>
