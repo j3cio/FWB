5870cf124ecb7546692c3584c7eea8ea910d4b2f
Brandon Lee <brandonlee2543@hotmail.com>
2024-03-07 08:48:39 -0400

    Testing  our loading rendering process

--- fwb/app/(auth)/(routes)/groups/[group_id]/loading.tsx
+++ fwb/app/(auth)/(routes)/groups/[group_id]/loading.tsx
@@ -1,4 +1,4 @@
-import { Box, Container } from "@mui/material";
+import { Box, Container, Skeleton } from "@mui/material";
 import NavBarSkeleton from "../../../../../components/ui/skeletons/variants/NavBarSkeleton";
 import GroupDetailsSection from "@/components/ui/privategroups/groupdetailspage/GroupDetailsSection";
 import GroupTabsSkeleton from "@/components/ui/skeletons/variants/GroupTabsSkeleton";
@@ -8,18 +8,11 @@ const Loading = () => {
   return (
     <Box sx={{ backgroundColor: "#1A1A23" }}>
       <Container disableGutters maxWidth="lg">
-       <NavBarSkeleton />
+        <NavBarSkeleton />
         <Box sx={{ position: "relative", paddingTop: "156px", zIndex: 0 }}>
-          <GroupDetailsSection
-            userData={{ success: true, users: [] }}
-            groupData={{
-              id: "",
-              name: "Loading...",
-              users: [],
-              discounts: [],
-              admins: [],
-            }}
-          />
+          <div className="h-2/3 w-full bg-white my-10 flex flex-col">
+            <Skeleton />
+          </div>
           <GroupTabsSkeleton />
         </Box>
       </Container>
