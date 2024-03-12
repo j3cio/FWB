680179d675249fe6a514ea8c0393d0670f7b917d
Brandon Lee <brandonlee2543@hotmail.com>
2024-03-12 07:29:32 -0400

    Extracted code to become a component

--- fwb/app/(auth)/(routes)/profile/Profile.tsx
+++ fwb/app/(auth)/(routes)/profile/Profile.tsx
@@ -1,5 +1,5 @@
 "use client";
-import BlueArrowForward from "@/components/ui/profile/BlueArrowForward";
+
 import Navbar from "@/components/ui/profile/profile_navbar";
 import WhiteArrowForward from "@/components/ui/profile/WhiteArrowForward";
 import { Box, Button, Container } from "@mui/material";
@@ -15,6 +15,7 @@ import { useState } from "react";
 import SaveIcon from "../../../../components/ui/profile/icons/save.svg";
 import { UserData } from "../../../types/types";
 import EditProfileModal from "./EditProfileModal";
+import CreateDiscountCard from "@/components/ui/intakeform/CreateDiscountCard";
 
 function Profile({ userData }: { userData: UserData }) {
   // It is hard to use the theme colors if they are not a specific MUI component, some colors are not showing up
@@ -87,32 +88,8 @@ function Profile({ userData }: { userData: UserData }) {
             </div>
             {/*Bargains div*/}
             <div className="flex flex-col w-full grow gap-6">
-              <a href="/intakeform">
-                <div className="pb-[27%] flex rounded-3xl items-center justify-center relative z-0 bg-no-repeat bg-center bg-contain bg-[url('/profileBanner.svg')]">
-                  <div className="absolute inset-0 flex items-center justify-center">
-                    <div className="flex flex-row-reverse w-5/6 mr-28">
-                      <Button
-                        endIcon={<BlueArrowForward />}
-                        variant="contained"
-                        sx={{
-                          borderRadius: 28,
-                          borderStyle: "solid",
-                          borderColor: "white",
-                          borderWidth: 2,
-                          bgcolor: `${theme.palette.secondary.light}`,
-                          color: `${theme.palette.primary.dark}`,
-                          ":hover": {
-                            bgcolor: `${theme.palette.secondary.light}`, // Hover background color
-                            color: `${theme.palette.primary.dark}`, // Hover text color
-                          },
-                        }}
-                      >
-                        Share your discount
-                      </Button>
-                    </div>
-                  </div>
-                </div>
-              </a>
+
+              <CreateDiscountCard />
               <div className="flex h-2/5 gap-6">
                 <a href="profile" className="flex flex-1 bg-white rounded-3xl items-center h-[126px]">
                   <div className="flex flex-col mx-6">
