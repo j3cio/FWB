45d1c4223fd865165a96f585ca6abe395717865a
Brandon Lee <brandonlee2543@hotmail.com>
2024-03-19 15:29:50 -0400

    Shifted  our discount retrieval logic to utils folder for reusability

---
+++ fwb/app/api/discounts/utils/get_discounts.ts
@@ -0,0 +1,46 @@
+import { auth } from '@clerk/nextjs'
+
+export async function getDiscount(discount_id: string) {
+  const bearer_token = await auth().getToken({ template: 'testing_template' })
+  const supabase_jwt = await auth().getToken({ template: 'supabase' })
+
+  if (!supabase_jwt) {
+    console.warn('Not signed in')
+    return
+  }
+
+  var myHeaders = new Headers()
+  myHeaders.append('supabase_jwt', supabase_jwt)
+  myHeaders.append('Authorization', `Bearer ${bearer_token}`)
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+  }
+  try {
+    if (discount_id !== '') {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/tempdiscounts?discount_id=${discount_id}`,
+        requestOptions
+      )
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+      const result = await response.json()
+      return result.data[0] // This returns the result object
+    }
+    return null // Explicitly added null instead of undefined or empty object to show that this is intended. API fix should be considered however, discount_id should never be ''.
+  } catch (error) {
+    console.error('Error fetching data: ', error)
+    throw error // This re-throws the error to be handled by the caller
+  }
+}
+
+export async function getAllDiscountsData(discount_ids: string[]) {
+  const promises = discount_ids.map((discount_id: string, key: number) =>
+    getDiscount(discount_id)
+  )
+  const results = await Promise.all(promises)
+  const filteredResults = results.filter((result) => result !== null)
+
+  return filteredResults
+}
