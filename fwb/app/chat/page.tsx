32d4a40ffa067488cb15e89d2f64f72e8bd855e3
Brandon Lee <brandonlee2543@hotmail.com>
2024-03-20 08:48:35 -0400

    Added navbar and basic props to fit res of app's design

--- fwb/app/chat/page.tsx
+++ fwb/app/chat/page.tsx
@@ -23,12 +23,18 @@ import { useState } from 'react'
 import RightGroup from '@/components/ui/message/RightGroup'
 import RightGeneral from '@/components/ui/message/RightGeneral'
 import { Container } from '@mui/material'
+import { useRouter } from 'next/navigation'
+
 //random
 
 export default function ChatPage() {
+  const [companyQuery, setCompanyQuery] = useState('')
+  const [tab, setTab] = useState<'general' | 'groups'>('general')
+
+  const router = useRouter()
   const chatClient = useIntitialChatClient()
   const { user } = useUser()
-  const [tab, setTab] = useState<'general' | 'groups'>('general')
+
   if (!chatClient || !user) {
     return (
       <div className="h-screen flex items-center justify-center">
@@ -37,25 +43,30 @@ export default function ChatPage() {
     )
   }
 
+  const handleSearch = (companyQuery: any) => {
+    const url = `/explore?company=${companyQuery}`
+    router.push(url)
+  }
+
   return (
     // <div className="w-8/12 h-screen mr-20">
 
     <div
       style={{
         background: '#1A1A23',
-        paddingBottom: '80px',
         minHeight: '100dvh',
       }}
     >
-      <Navbar />
+      <Navbar
+        handleSearch={handleSearch}
+        companyQuery={companyQuery}
+        setCompanyQuery={setCompanyQuery}
+      />
       <div className="flex flex-col items-center">
         <Chat client={chatClient}>
           {/* <Chat theme={"str-chat__theme-dark"} client={chatClient}> */}
           {/* The channel list shows only channels that the currently loggeed in user is a member (filters prop) */}
-          <div
-            className="flex flex-row justify-center w-full px-14 h-full gap-6"
-            style={{ marginTop: '40px' }}
-          >
+          <div className="flex flex-row justify-center w-full pt-2 px-14 gap-6">
             <ChatSideBar user={user} />
             <ChatChannel />
           </div>
