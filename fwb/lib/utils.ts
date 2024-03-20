59f03a8037f8b7080293d7956fb9772d1588d9ab
Brandon Lee <brandonlee2543@hotmail.com>
2024-03-20 10:50:41 -0400

    Extracted fuzzySearch functionality to util file

--- fwb/lib/utils.ts
+++ fwb/lib/utils.ts
@@ -1,6 +1,29 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import Fuse from 'fuse.js'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+interface fuzzySearchParams {
+  queryString: string
+  searchIndex: any[]
+  keys?: string[]
+}
+
+export const fuzzySearch = async ({
+  queryString,
+  searchIndex,
+  keys = ['name'],
+}: fuzzySearchParams) => {
+  const fuseOptions = {
+    keys,
+  }
+  const fuse = new Fuse(searchIndex, fuseOptions)
+
+  const searchResults = await fuse.search(queryString)
+  console.log({ searchIndex })
+  console.log({ queryString, searchResults })
+  return searchResults
 }
