34c1d19094089251a0dd4678ddadfb743247ec22
Brandon Lee <brandonlee2543@hotmail.com>
2024-03-29 15:58:37 -0400

    Styling change

--- fwb/components/ui/chat/mobile/MobileChatNavigation.tsx
+++ fwb/components/ui/chat/mobile/MobileChatNavigation.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+import Image from 'next/image'
 import { useChatContext } from 'stream-chat-react'
 
 import AddFriendsIcon from '../icons/AddFriendsIcon'
@@ -7,17 +7,33 @@ import BackArrowIcon from '../icons/BackArrowIcon'
 import EditIcon from '../icons/EditIcon'
 
 const MobileChatNavigation = () => {
-  const { setActiveChannel, channel } = useChatContext()
+  const { setActiveChannel, channel, channelsQueryState } = useChatContext()
+
+  const recipient = channel?.state.members
+  const membersArray = recipient && Object.values(recipient)
+  const memberWithRoleMember =
+    membersArray && membersArray.find((member) => member.role === 'member')
+
+  const recipientName = memberWithRoleMember && memberWithRoleMember.user?.name
 
   return (
     <nav className="flex font-urbanist justify-between py-[28px]">
       <div
-        className="flex items-center cursor-pointer"
+        className="flex items-center cursor-pointer gap-2"
         onClick={() => setActiveChannel(undefined, undefined, undefined)}
       >
         <BackArrowIcon />
+
+        {channel && memberWithRoleMember ? (
+          <Image
+            src={memberWithRoleMember?.user?.image || ''}
+            width={24}
+            height={24}
+            alt={`User ${recipientName} Profile Picture`}
+          />
+        ) : null}
         <p className="text-white font-semibold">
-          {channel ? 'Messages' : 'Messages'}
+          {channel ? (recipientName ? recipientName : 'General') : 'Messages'}
         </p>
       </div>
       <div className="flex items-center gap-3 cursor-pointer">
