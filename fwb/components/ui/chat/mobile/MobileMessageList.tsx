f2eb3e10b7ff745c04df66bcae3a9c1876165aea
Brandon Lee <brandonlee2543@hotmail.com>
2024-03-29 15:58:29 -0400

    Removed loadingINdicator

--- fwb/components/ui/chat/mobile/MobileMessageList.tsx
+++ fwb/components/ui/chat/mobile/MobileMessageList.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Channel, MessageList, MessageInput } from 'stream-chat-react'
+import { MessageList, MessageInput, Channel } from 'stream-chat-react'
 
 const MobileMessageList = () => {
   return (
     <Channel>
       <section className="flex flex-col">
-        <MessageList />
+        {/* setting loadingMore to false gets rid of the loading indicator */}
+        <MessageList loadingMore={false} />
+
         <div className="fixed bottom-0 w-full px-4">
           <MessageInput />
         </div>
