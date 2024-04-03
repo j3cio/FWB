a2aa1148b636b5a883b8b043a30c9641edc42913
Brandon Lee <brandonlee2543@hotmail.com>
2024-04-03 13:28:03 -0400

    Update page.tsx

--- fwb/app/(auth)/(routes)/groups/[group_id]/page.tsx
+++ fwb/app/(auth)/(routes)/groups/[group_id]/page.tsx
@@ -133,7 +133,7 @@ const Page = async ({ params }: { params: { group_id: string } }) => {
       }}
     >
       <Container disableGutters maxWidth="lg">
-        <div className="bg-[#1A1A23' min-h-[80px]">
+        <div className="bg-[#1A1A23' min-h-[180px]">
           <SingleGroupNavbar />
         </div>
         <Box
