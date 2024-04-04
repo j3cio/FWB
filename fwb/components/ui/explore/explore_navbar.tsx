60353a3b0bee1020b4c35fd9eac47b253a7f14a9
Derick Young <young.derickj@gmail.com>
2024-04-04 16:28:14 -0600

    Allow navbar to show without being signed in

--- fwb/components/ui/explore/explore_navbar.tsx
+++ fwb/components/ui/explore/explore_navbar.tsx
@@ -345,7 +345,225 @@ const Navbar: React.FC<NavbarProps> = ({
         </AppBar>
       </ThemeProvider>
     )
+  } else {
+    return (
+      <ThemeProvider theme={theme}>
+        <AppBar
+          position="static"
+          sx={{
+            backgroundColor: '#1A1A23',
+            boxShadow: 'none',
+            paddingY: '32px',
+            paddingX: '18px',
+            position: 'sticky',
+            top: 0,
+            zIndex: 1,
+            height: '112px',
+            width: '100%',
+          }}
+        >
+          <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+            <button onClick={() => router.push('/profile')}>
+              <Image 
+                  priority
+                  className='mr-[4.6vw] w-44 h-full' 
+                  src='/fwb_logo.png' 
+                  alt="logo" 
+                  width={900} 
+                  height={0} 
+              />
+            </button>
+            <Toolbar
+              disableGutters
+              variant="dense"
+              sx={{
+                display: 'flex',
+                gap: '24px',
+                height: '9.6px',
+                flexGrow: 1,
+              }}
+            >
+              <SearchBar
+                handleSearch={handleSearch}
+                companyQuery={companyQuery}
+                setCompanyQuery={setCompanyQuery}
+              />
+              <Tooltip
+                title="Explore"
+                slotProps={{
+                  popper: {
+                    modifiers: [
+                      {
+                        name: 'offset',
+                        options: {
+                          offset: [0, -14],
+                        },
+                      },
+                    ],
+                  },
+                }}
+              >
+                <IconButton
+                  color="inherit"
+                  onClick={clearSearch}
+                  sx={{
+                    padding: '9.6px',
+                    borderRadius: '50%',
+                    border: '2px solid white',
+                    borderColor: '#8e94e9',
+                    backgroundColor: '#1a1a23',
+                    '&:hover': {
+                      backgroundColor: '#8e94e9',
+                      borderColor: '#8e94e9',
+                    },
+                  }}
+                >
+                  <Image
+                    src={discountIcon}
+                    alt="Explore"
+                    style={{
+                      width: '28.8px',
+                      height: '28.8px',
+                    }}
+                  />
+                </IconButton>
+              </Tooltip>
+              <Tooltip
+                title="Groups"
+                slotProps={{
+                  popper: {
+                    modifiers: [
+                      {
+                        name: 'offset',
+                        options: {
+                          offset: [0, -14],
+                        },
+                      },
+                    ],
+                  },
+                }}
+              >
+                <IconButton
+                  color="inherit"
+                  onClick={() => router.push('/groups')}
+                  sx={{
+                    padding: '9.6px',
+                    borderRadius: '50%',
+                    border: '2px solid white',
+                    '&:hover': {
+                      backgroundColor: '#8e94e9',
+                      borderColor: '#8e94e9',
+                    },
+                  }}
+                >
+                  <Image
+                    src={groupIcon}
+                    alt="Groups"
+                    style={{
+                      width: '28.8px',
+                      height: '28.8px',
+                    }}
+                  />
+                </IconButton>
+              </Tooltip>
+              <Tooltip
+                title="Messages"
+                slotProps={{
+                  popper: {
+                    modifiers: [
+                      {
+                        name: 'offset',
+                        options: {
+                          offset: [0, -14],
+                        },
+                      },
+                    ],
+                  },
+                }}
+              >
+                <IconButton
+                  color="inherit"
+                  onClick={() => router.push('/chat')}
+                  sx={{
+                    padding: '9.6px',
+                    borderRadius: '50%',
+                    border: '2px solid white',
+                    '&:hover': {
+                      backgroundColor: '#8e94e9',
+                      borderColor: '#8e94e9',
+                    },
+                  }}
+                >
+                  <Image
+                    src={messageIcon}
+                    alt="Messages"
+                    style={{
+                      width: '28.8px',
+                      height: '28.8px',
+                    }}
+                  />
+                </IconButton>
+              </Tooltip>
+              {/* <IconButton
+                id="user-button"
+                aria-controls={open ? 'user-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+                sx={{
+                  backgroundColor: '#DAE3EA',
+                  padding: '9.6px',
+                  overflow: 'hidden',
+                  borderRadius: '50%',
+                  border: '2px solid white',
+                  '&:hover': {
+                    backgroundColor: '#8e94e9',
+                    borderColor: '#8e94e9',
+                  },
+                }}
+              >
+                <div
+                  style={{
+                    width: '28.8px',
+                    height: '28.8px',
+                  }}
+                >
+                  <Image src={user.imageUrl} alt={avatar} fill />
+                </div>
+              </IconButton> */}
+              <Menu
+                id="user-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                disableScrollLock={true}
+                MenuListProps={{
+                  'aria-labelledby': 'user-button',
+                }}
+                sx={{
+                  width: '500px',
+                  marginTop: '10px',
+                }}
+              >
+                <MenuItem onClick={handleProfileClick}>
+                  <PersonIcon style={{ marginRight: '8px' }} />
+                  View Profile
+                </MenuItem>
+                <MenuItem onClick={() => signOut(() => router.push('/'))}>
+                  <LogoutIcon style={{ marginRight: '8px' }} />
+                  Logout
+                </MenuItem>
+              </Menu>
+            </Toolbar>
+          </Box>
+        </AppBar>
+      </ThemeProvider>
+
+    )
   }
+
+
+
 }
 
 export default Navbar
