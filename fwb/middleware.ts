185f3a368d5141d37ae180aca23c05dec8654ae8
Derick Young <young.derickj@gmail.com>
2024-04-04 16:28:00 -0600

    Create public explore page

--- fwb/middleware.ts
+++ fwb/middleware.ts
@@ -5,7 +5,7 @@ import { NextResponse } from 'next/server'
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 
-const publicRoutes = ['/', '/forgotpassword', '/sign-in', 'sign-up']
+const publicRoutes = ['/', '/forgotpassword', '/sign-in', '/sign-up', '/public-explore']
 const protectedRoutes = ['/profile', '/api', '/api/discounts', '/dashboard', '/fre1']
 
 export default authMiddleware({
