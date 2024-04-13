681173d932e412c249e3b1b2ac8fc0a0c01fa1d1
Brandon Lee <brandonlee2543@hotmail.com>
2024-04-12 21:07:52 -0400

    Add naive approach for getting auth code

--- fwb/components/ui/linkedin/VerifyEmploymentCard.tsx
+++ fwb/components/ui/linkedin/VerifyEmploymentCard.tsx
@@ -5,11 +5,49 @@ import YellowVerifyIcon from './icons/YellowVerifyIcon'
 import NavigationArrowIcon from './icons/NavigationArrowIcon'
 
 const VerifyEmploymentCard = () => {
+  const linkedinClientId = process.env.NEXT_PUBLIC_LINKEDIN_CLIENT_ID
+  const linkedinClientSecret = process.env.NEXT_PUBLIC_LINKEDIN_CLIENT_SECRET
+  // we can change this as needed ofc, something like this: const redirect_UI = process.env.NODE_ENV !== "production" ?  'http://localhost:3000/linkedin' : "https://app.makefwb.com/linkedin", im just leaving it to localhost for debugging
+  const redirect_UI =
+    process.env.NODE_ENV !== 'production'
+      ? 'http://localhost:3000/linkedin'
+      : 'https://fwb-git-brandon-linkedin-button-j3cs-projects-612eefdf.vercel.app/linkedin'
+
   const router = useRouter()
 
-  const handleVerification = () => {
+  const handleVerification = async () => {
+    console.log({ linkedinClientId, linkedinClientSecret })
+    if (!linkedinClientId || !linkedinClientSecret) {
+      console.error('ENV VARIABLES MISMATCH')
+      return { status: 500 }
+    }
     console.log('firing')
-    // router.push("")
+    const authCodeParams = new URLSearchParams({
+      response_type: 'code',
+      client_id: linkedinClientId,
+      state: crypto.randomUUID(), // This is used to prevent CSRF, so it being random is important
+      scope: 'r_basicprofile', // You normally use a chain of scopes, but for now this seems like all we need
+      redirect_uri: redirect_UI,
+    })
+    // We need to first get an authorization code before we can do anything else.
+    const authorizationCode = await fetch(
+      `https://www.linkedin.com/oauth/v2/authorization?${authCodeParams}`,
+      {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      }
+    )
+    // if (!authorizationCode) {
+    //   console.log('something broke')
+    //   return { error: 'Authorization Code Not found', status: 500 }
+    // }
+    // const data = await authorizationCode.json()
+    // Once we get an auth code, our next step is to exchange our Authorization code for an access token
+    if (authorizationCode.ok) {
+      console.log({ authorizationCode: await authorizationCode.json() })
+    }
+    return authorizationCode
   }
   return (
     // Not responsive for the moment, this is just to save someone time when we implement this properly. Should be easy to implement since the width is hardcoded at the parent level.
