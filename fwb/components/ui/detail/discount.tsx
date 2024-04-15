3c3488d5d1b802963218d10ef99314721a5d31ed
Derick Young <young.derickj@gmail.com>
2024-04-15 00:44:12 -0600

    Allow users to message the discount owner

--- fwb/components/ui/detail/discount.tsx
+++ fwb/components/ui/detail/discount.tsx
@@ -1,11 +1,71 @@
 'use client'
 
 import { DiscountDataDetail } from '@/app/types/types'
+import { useAuth } from '@clerk/nextjs'
+import { Chat, LoadingIndicator, useChatContext } from 'stream-chat-react'
+import { useRouter } from 'next/navigation'
+import useIntitialChatClient from '@/app/chat/useIntializeChatClient'
+import { useUser } from '@clerk/nextjs'
 
-export default function ProductCard(
-  { data }: { data: DiscountDataDetail },
-  { key }: { key: number }
-) {
+const MessageButton = ({ data }: { data: DiscountDataDetail },{ key }: { key: number }) => {
+  const { userId } = useAuth()
+  const { client } = useChatContext()
+  const router = useRouter()
+
+    // This function takes in the userId of the person you are starting a chat with and will create a chat with them.
+    async function startChat(userId: any) {
+      try {
+        const channel = client.channel('messaging', {
+          members: [userId, data.user_id],
+        })
+        console.log(channel)
+        await channel.create()
+        router.push('/chat')
+      } catch (error) {
+        console.log('Error creating channel')
+        console.error(error)
+      }
+    }
+
+  return (
+    <div>
+            <div className="mr-[16px] my-auto">
+              <button onClick={() => startChat(userId)}>
+              <svg
+                width="48"
+                height="48"
+                viewBox="0 0 50 50"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  x="1"
+                  y="1"
+                  width="45.9996"
+                  height="45.9996"
+                  rx="22.9998"
+                  stroke="#8E94E9"
+                  strokeWidth="2"
+                />
+                <path
+                  d="M33.5998 11.9998H14.4C13.08 11.9998 12.012 13.0797 12.012 14.3997L12 35.9996L16.8 31.1996H33.5998C34.9198 31.1996 35.9998 30.1196 35.9998 28.7996V14.3997C35.9998 13.0797 34.9198 11.9998 33.5998 11.9998Z"
+                  fill="#8E94E9"
+                />
+              </svg>
+              </button>
+            </div>
+    </div>
+  )
+}
+
+
+export default function ProductCard({ data }: { data: DiscountDataDetail },{ key }: { key: number }) {
+
+  const chatClient = useIntitialChatClient()
+  const { userId } = useAuth()
+  const { user } = useUser()
+
+
   const copyShareURL = () => {
     const currentURL = window.location.href
     // Copy URL to clipboard
@@ -19,6 +79,16 @@ export default function ProductCard(
       })
   }
 
+
+  if (!chatClient || !user) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <LoadingIndicator size={40} />
+      </div>
+    )
+  }
+
+  console.log(data.user_id)
   return (
     <div className="mx-[120px] flex flex-row relative mb-[32px]">
       <div className="w-[20%] bg-[#8E94E9] text-white flex rounded-l-[25px]">
@@ -46,29 +116,10 @@ export default function ProductCard(
             </div>
           </div>
           <div className="flex h-auto my-auto cursor-pointer">
-            <div className="mr-[16px] my-auto">
-              <svg
-                width="48"
-                height="48"
-                viewBox="0 0 50 50"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  x="1"
-                  y="1"
-                  width="45.9996"
-                  height="45.9996"
-                  rx="22.9998"
-                  stroke="#8E94E9"
-                  strokeWidth="2"
-                />
-                <path
-                  d="M33.5998 11.9998H14.4C13.08 11.9998 12.012 13.0797 12.012 14.3997L12 35.9996L16.8 31.1996H33.5998C34.9198 31.1996 35.9998 30.1196 35.9998 28.7996V14.3997C35.9998 13.0797 34.9198 11.9998 33.5998 11.9998Z"
-                  fill="#8E94E9"
-                />
-              </svg>
-            </div>
+            {/* Chat message link */}
+            <Chat client={chatClient}>
+           <MessageButton data={data}/>
+            </Chat>
             {/* This is the sharable link */}
             <div className="mr-[16px] my-auto cursor-pointer">
               <button onClick={copyShareURL}>
