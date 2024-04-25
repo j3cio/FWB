d85d68eeff9c199db8c96057892a3bb6d5be287b
Brandon Lee <brandonlee2543@hotmail.com>
2024-04-25 04:11:24 -0400

    Added redirection to sidebar

--- fwb/components/ui/navbar/mobile/MobileSideBarButton.tsx
+++ fwb/components/ui/navbar/mobile/MobileSideBarButton.tsx
@@ -48,19 +48,20 @@ const MobileSideBarButton = () => {
       >
         <div className="mobile__sidebar flex h-dvh w-[60vw] flex-col bg-[#1A1A23] px-4">
           <section className="my-6">
-            <div
-              className="border-bg-white flex items-center gap-4"
-              onClick={() => handleClose()}
-            >
-              <MobileHamburgerIcon />
-              <Image
-                priority
-                src="/fwb_logo.png"
-                alt="logo"
-                width={110}
-                height={0}
-              />
-            </div>
+            <article className=" flex items-center gap-4">
+              <div className="border-bg-white" onClick={() => handleClose()}>
+                <MobileHamburgerIcon />
+              </div>
+              <Link href="/profile" onClick={() => handleClose()}>
+                <Image
+                  priority
+                  src="/fwb_logo.png"
+                  alt="logo"
+                  width={110}
+                  height={0}
+                />
+              </Link>
+            </article>
             <div className="mt-[14px] h-[1.5px] bg-white opacity-25" />
           </section>
 
