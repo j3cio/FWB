125c1ec2392b7942089cf143302735c9fe48d42a
Emmanuel Ayo <emmieaddiz@gmail.com>
2024-05-17 12:58:34 +0100

    invite modal (group details page) update.

--- fwb/components/ui/privategroups/groupdetailspage/GroupInviteModal.tsx
+++ fwb/components/ui/privategroups/groupdetailspage/GroupInviteModal.tsx
@@ -1,49 +1,105 @@
 import { useRouter } from 'next/navigation'
 import { useState, FormEvent, KeyboardEvent } from 'react'
-import { Modal } from '@mui/material'
+import { Button, Modal } from '@mui/material'
 import axios from 'axios'
-import CloseButton from '../icons/close_24px.svg'
-import { Image } from 'next/dist/client/image-component'
+import { Box, FormControl, FormLabel, IconButton, Input, MenuItem, Select, Stack, Typography } from '@mui/material'
+import { ToastContainer, toast } from 'react-toastify'
+import CloseIcon from '../../chat/icons/CloseIcon'
+import { SelectChangeEvent } from '@mui/material/Select';
+import 'react-toastify/dist/ReactToastify.css'
+import EndArrowWhite from '../icons/EndArrowWhite'
+import TrashIcon from '../icons/TrashIcon'
+import Image from 'next/image'
 
 type GroupInviteModalProps = {
   isOpen: boolean
   onClose(): void
 }
 
+type User = {
+  email: string
+  role: string
+}
+
 const GroupInviteModal = ({ isOpen, onClose }: GroupInviteModalProps) => {
+  const initialData = {
+    email: '',
+    role: 'member'
+  }
   const router = useRouter()
   const [refresh, setRefresh] = useState(true)
-  const [emailInput, setEmailInput] = useState<string>('')
-  const [emailAddresses, setEmailAddresses] = useState<string[]>([])
-  const [errorMessage, setErrorMessage] = useState('')
+  const [input, setInput] = useState<User>(initialData)
+  const [inviteList, setInviteList] = useState<User[]>([])
+
+  const validateEmail = (email: string): boolean => {
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    if (!email.match(emailRegex)) {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  const handleAddUser = (user: User) => {
+    let trimmedEmail = user.email.trim()
+    let emails: string[] = []
+    const GetEmails = () => inviteList.map((user) => (emails.push(user.email)))
+    GetEmails()
+
+    // check for empty input
+    if (!trimmedEmail) {
+      toast.error('input a user email..')
+      return
+    }
+    // check for duplicate emails
+    if (emails.includes(trimmedEmail)) {
+      toast.error('email already added!')
+      return
+    }
+    //validate the email before adding
+    if (!validateEmail(trimmedEmail)) {
+      toast.error('not a valid email')
+      return
+    } else {
+      setInviteList((prev) => ([...prev, user]))
+      setInput(initialData)
+    }
+  }
+
+  const handleRoleChange = (event: SelectChangeEvent, index: number) => {
+
+    function update(arr: User[], id: number, updatedData: Partial<User>): User[] {
+      return arr.map((item, index) => (index === id ?  { ...item, ...updatedData } : item))
+    }
+
+    const updatedEmails = update(inviteList, index, {role: event.target.value})
+    setInviteList([...updatedEmails])
+  }
 
   //adding emails
   const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
       e.preventDefault()
-      if (emailInput.trim() !== '') {
-        setEmailAddresses((prevEmails) => [...prevEmails, emailInput])
-        setEmailInput('')
-      }
+      handleAddUser(input)
     }
   }
 
   //error message if input is empty
   const handleShare = async () => {
-    if (emailAddresses.length === 0) {
-      setErrorMessage('Please enter at least one email before sharing.')
+    if (inviteList.length === 0) {
+      toast.error('Please enter at least one email before sharing.')
       return
     }
 
     try {
       // Send emails
       const response = await axios.post('/api/invitations', {
-        emails: emailAddresses,
+        emails: inviteList,
       })
 
       // Reset state after sending emails
-      setEmailAddresses([])
-      setEmailInput('')
+      setInviteList([])
+      setInput(initialData)
       onClose()
     } catch (error) {
       console.error('Error sending email:', error)
@@ -52,7 +108,7 @@ const GroupInviteModal = ({ isOpen, onClose }: GroupInviteModalProps) => {
 
   //removing emails
   const handleRemoveEmail = (index: number) => {
-    setEmailAddresses((prevEmails) => {
+    setInviteList((prevEmails) => {
       const updatedEmails = [...prevEmails]
       updatedEmails.splice(index, 1)
       return updatedEmails
@@ -65,12 +121,12 @@ const GroupInviteModal = ({ isOpen, onClose }: GroupInviteModalProps) => {
 
     try {
       const response = await axios.post('/api/invitations', {
-        emails: emailAddresses,
+        emails: inviteList,
       })
 
       // 이메일 전송 후 상태 초기화 또는 다른 작업 수행
-      setEmailAddresses([])
-      setEmailInput('')
+      setInviteList([])
+      setInput(initialData)
     } catch (error) {
       console.error('Error sending email:', error)
     }
@@ -93,160 +149,163 @@ const GroupInviteModal = ({ isOpen, onClose }: GroupInviteModalProps) => {
       }}
     >
       <div
-        className="modal-content relative sm-max:w-[80%] xs-max:h-screen xs-max:w-full xs-max:rounded-none xs-max:border-none"
+        className="modal-content relative sm-max:w-[80%] xs-max:h-screen xs-max:w-full xs-max:rounded-none xs-max:border-none xxs-max:h-screen xxs-max:w-full xxs-max:rounded-none xxs-max:border-none"
         style={{
           borderRadius: '40px',
           border: '2px solid var(--Neutral-000, #FFF)',
           background: '#8E94E9',
           boxShadow: '0px 4px 4px 0px rgba(255, 255, 255, 0.25)',
-          backdropFilter: 'blur(100px)',
+          //backdropFilter: 'blur(100px)',
           padding: '20px',
           width: '788px',
           height: '600px',
         }}
       >
-        <div className="absolute right-5 top-5">
-          <button onClick={handleModuleClose}>
-            <Image src={CloseButton} alt="Close Button" />
-          </button>
-        </div>
-        <div className="flex justify-center">
-          <h1
-            className="font-urbanist font-medium text-white"
-            style={{
-              color: 'var(--Neutral-000, #FFF)',
-              textAlign: 'center',
-              marginTop: '88px',
-              fontSize: '32px',
-              fontStyle: 'normal',
-              fontWeight: '600',
-              marginBottom: '40px',
-            }}
+        <Button
+            className="absolute right-0 z-[99] top-2 text-xl font-medium text-white"
+            onClick={onClose}
+          >
+              <Image
+                className="h-8 w-8"
+                src="/groups/icon-close.svg"
+                height={0}
+                width={0}
+                alt="icon-close"
+              />
+            </Button>
+       <>
+        <FormLabel className="flex min-w-[50vw] flex-col px-[5%] sm:px-[15%] font-urbanist text-white sm-max:min-w-[60vw] xs-max:min-w-[80vw] xxs-max:min-w-[80vw]">
+          <Typography
+            className='font-urbanist text-white font-medium text-center mt-6 text-3xl py-12'
           >
             Invite Members
-          </h1>
-        </div>
-        <form
-          id="invitations"
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            flexDirection: 'column',
-            backgroundColor: 'white',
-            borderRadius: '10px',
-            padding: '12px',
-          }}
-          onSubmit={handleSubmit}
-        >
-          <div className="flex flex-wrap">
-            {emailAddresses.map((email, index) => (
-              <span
-                key={index}
-                className="font-urbanist font-medium text-white"
-                style={{
-                  backgroundColor: 'var(--neutral-200, #adb4d2)',
-                  color: 'white',
-                  display: 'flex',
-                  padding: '1px 6px',
-                  gap: '4px',
-                  height: '28px',
-                  borderRadius: '100px',
-                  fontFamily: 'Urbanist',
-                  fontSize: '16px',
-                  fontWeight: '400',
-                  lineHeight: '150%',
-                  marginRight: '5px',
-                  marginBottom: '5px',
-                }}
-              >
-                <div className="flex">
-                  <div
-                    className="font-urbanist font-medium text-white"
-                    style={{
-                      backgroundColor: 'var(--neutral-200, #adb4d2)',
-                      color: 'white',
-                      display: 'flex',
-                      padding: '1px 6px',
-                      gap: '4px',
-                      borderRadius: '100px',
-                      fontFamily: 'Urbanist',
-                      fontSize: '16px',
-                      fontWeight: '400',
-                      lineHeight: '150%',
-                      marginRight: '5px',
-                      marginBottom: '5px',
-                    }}
-                  >
-                    {email}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="17"
-                      viewBox="0 0 16 17"
-                      fill="none"
-                      style={{
-                        marginTop: '4px',
-                        marginLeft: '5px',
-                      }}
-                      onClick={() => handleRemoveEmail(index)}
-                    >
-                      <path
-                        d="M12.2005 4.02258C12.0759 3.89774 11.9068 3.82759 11.7305 3.82759C11.5541 3.82759 11.385 3.89774 11.2605 4.02258L8.00047 7.27591L4.74047 4.01591C4.61591 3.89108 4.44681 3.82092 4.27047 3.82092C4.09412 3.82092 3.92502 3.89108 3.80047 4.01591C3.54047 4.27591 3.54047 4.69591 3.80047 4.95591L7.06047 8.21591L3.80047 11.4759C3.54047 11.7359 3.54047 12.1559 3.80047 12.4159C4.06047 12.6759 4.48047 12.6759 4.74047 12.4159L8.00047 9.15591L11.2605 12.4159C11.5205 12.6759 11.9405 12.6759 12.2005 12.4159C12.4605 12.1559 12.4605 11.7359 12.2005 11.4759L8.94047 8.21591L12.2005 4.95591C12.4538 4.70258 12.4538 4.27591 12.2005 4.02258Z"
-                        fill="white"
-                      />
-                    </svg>
-                  </div>
-                </div>
-              </span>
-            ))}
-          </div>
-          <input
-            type="text"
-            style={{
-              background: 'var(--neutral-000, #fff)',
-              outline: 'none',
-            }}
-            placeholder="Invite your friends..."
-            id="emailInput"
-            value={emailInput}
-            onChange={(e) => setEmailInput(e.target.value)}
-            onKeyDown={handleKeyDown}
-          />
-        </form>
-        {errorMessage && (
-          <div className="error-message" style={{ color: 'white' }}>
-            {errorMessage}
-          </div>
-        )}
-        <div className="mt-0 flex flex-col items-center">
-          <button
-            className="font-urbanist font-medium"
-            style={{
-              borderRadius: '30px',
-              background: 'var(--yellow-300, #f6ff82)',
+          </Typography>
+          <Box
+            sx={{
               display: 'flex',
-              width: '367px',
-              height: '48px',
-              padding: '10px 24px',
-              justifyContent: 'center',
-              gap: '8px',
-              color: 'var(--purple-300, #8e94e9)',
-              textAlign: 'center',
-              fontFamily: 'Urbanist',
-              fontSize: '20px',
-              fontStyle: 'normal',
-              fontWeight: '700',
-              lineHeight: '125%', // You may need to adjust this value to match your design
-              letterSpacing: '0.4px',
-              marginTop: '24px',
-              marginBottom: '116px',
+              alignItems: 'right',
+              borderRadius: '100px',
+              backgroundColor: 'white',
+              flexGrow: 1,
+              marginTop: 1,
+              border: 'none',
             }}
-            type="button"
-            onClick={handleShare}
           >
-            Send Invitation
-          </button>
+            <Input
+              fullWidth
+              className="w-full border-none bg-white p-2 font-urbanist before:border-none after:border-0 focus:outline-none"
+              placeholder="Enter email"
+              autoFocus
+              required
+              value={input.email}
+              onKeyDown={handleKeyDown}
+              onChange={(e) => setInput({ ...input, email: e.target.value.trim()})}
+              style={{
+                flex: 1,
+                height: '48px',
+                borderRadius: '25px 0 0 25px',
+                justifyContent: 'center',
+              }}
+              sx={{
+                '& .MuiOutlinedInput-notchedOutline': { border: 'none' },
+                '&.MuiFormControl-root': { alignItems: 'flex-start' },
+              }}
+            />
+            <IconButton
+              color="primary"
+              aria-label="search"
+              className='bg-black'
+              sx={{
+                backgroundColor: '#000',
+                padding: '10px',
+                border: 'none',
+                margin: '4px',
+                transition: 'backgroundColor 1s ease',
+                '&:hover': {
+                  backgroundColor: '#8e94e9',
+                },
+              }}
+            >
+              <EndArrowWhite />
+            </IconButton>
+          </Box>
+          <Box className="relative mt-3 min-h-[50vh] sm:h-[20vh] sm:min-h-[15vh] w-full py-2 pb-2 border-b border-[#fff]/50 font-urbanist overflow-y-auto">
+            <Stack
+              className='overflow-hidden' 
+              direction="column" 
+              flexWrap="nowrap" 
+              useFlexGap 
+              spacing={1}
+            >
+              {inviteList.map((user, index) => (
+                <Box
+                  className="flex w-full items-center font-urbanist lowercase text-white"
+                  key={index}
+                >
+                  <Typography className="font-urbanist text-base">
+                    {user.email}
+                  </Typography>
+                  <Box className="ml-auto flex items-center gap-3">
+                  <FormControl size='small' className="text-white border-none" sx={{ minWidth: 120 }}>
+                      <Select
+                        defaultValue='Member'
+                        className="text-white font-urbanist"
+                        value={user.role}
+                        onChange={(e) => handleRoleChange(e, index)}
+                        displayEmpty                     
+                        inputProps={{ 'aria-label': 'Without label' }}
+                      >
+                        <MenuItem className="font-urbanist" value={'member'}>Member</MenuItem>
+                        <MenuItem className="font-urbanist" value={'admin'}>Admin</MenuItem>
+                      </Select>
+                    </FormControl>
+                    <TrashIcon
+                      className=""
+                      onClick={() => handleRemoveEmail(index)}
+                    />
+                  </Box>
+                </Box>
+              ))}
+            </Stack>
+          </Box>
+          <div className="mt-0 flex flex-col items-center">
+            <button
+              className="font-urbanist font-medium"
+              style={{
+                borderRadius: '30px',
+                background: 'var(--yellow-300, #f6ff82)',
+                display: 'flex',
+                width: '367px',
+                height: '48px',
+                padding: '10px 24px',
+                justifyContent: 'center',
+                gap: '8px',
+                color: 'var(--purple-300, #8e94e9)',
+                textAlign: 'center',
+                fontFamily: 'Urbanist',
+                fontSize: '20px',
+                fontStyle: 'normal',
+                fontWeight: '700',
+                lineHeight: '125%', // You may need to adjust this value to match your design
+                letterSpacing: '0.4px',
+                marginTop: '24px',
+                marginBottom: '116px',
+              }}
+              type="button"
+              onClick={handleShare}
+            >
+              Send Invitation
+            </button>
         </div>
+        </FormLabel>
+        <ToastContainer
+          autoClose={3000}
+          toastClassName="bg-[#8E94E9]"
+          bodyClassName="text-white"
+          theme="colored"
+          position="top-center"
+        />
+      </>
       </div>
     </Modal>
   )
