514a447d94bd1873724aa834454398938d7d717f
Brandon Lee <brandonlee2543@hotmail.com>
2024-06-04 07:44:23 -0400

    formatting

--- fwb/app/(auth)/(routes)/profile/Profile.tsx
+++ fwb/app/(auth)/(routes)/profile/Profile.tsx
@@ -16,7 +16,6 @@ import EditProfileModal from './EditProfileModal'
 //import AvatarIcon from "@mui/material/Avatar";
 //import LinkedInIcon from "../../components/ui/profile/icons/linkedin.svg";
 
-
 import useIntitialChatClient from '@/app/chat/useIntializeChatClient'
 
 import { DiscountData, UserData } from '../../../types/types'
@@ -32,11 +31,11 @@ function Profile({ userData, discountData, isPublic }: ProfileProps) {
   // It is hard to use the theme colors if they are not a specific MUI component, some colors are not showing up
   const theme = useTheme() // To call useTheme you have to add "use client;" to the top of your file
   const { getToken } = useAuth()
-  //Intialize the user to be in GetStream db
+
+  // //Intialize the user to be in GetStream db
   const client = useIntitialChatClient()
 
   const { user } = useUser()
-
 
   const openEditProfileModal = () => {
     setIsEditProfileModalOpen(true)
@@ -67,7 +66,6 @@ function Profile({ userData, discountData, isPublic }: ProfileProps) {
               )}
               <div className="flex grow flex-col justify-center">
                 <div className="mb-[4px] text-[35px] font-semibold leading-none text-slate-200 sm-max:text-[28px] xs-max:text-[24px] xxs-max:text-[24px]">
-
                   {userData.users[0].username}
                 </div>
                 {userData.users[0].company && (
@@ -78,75 +76,83 @@ function Profile({ userData, discountData, isPublic }: ProfileProps) {
                     </div>
                   </div>
                 )}
-                {!isPublic ? <div className="my-2 flex gap-2">
-                  <Button
-                    endIcon={<WhiteArrowForward />}
-                    variant="contained"
-                    sx={{
-                      borderRadius: 28,
-                      borderStyle: 'solid',
-                      borderColor: 'white',
-                      borderWidth: 2,
-                      bgcolor: `${theme.palette.neutral.n900}`,
-                      color: `${theme.palette.common.white}`,
-                      ':hover': {
-                        bgcolor: `${theme.palette.neutral.n900}`, // Hover background color
-                        color: `${theme.palette.common.white}`, // Hover text color
-                      },
-                    }}
-                    onClick={openEditProfileModal}
-                  >
-                    Edit Profile
-                  </Button>
-                </div> : <div></div>}
+                {!isPublic ? (
+                  <div className="my-2 flex gap-2">
+                    <Button
+                      endIcon={<WhiteArrowForward />}
+                      variant="contained"
+                      sx={{
+                        borderRadius: 28,
+                        borderStyle: 'solid',
+                        borderColor: 'white',
+                        borderWidth: 2,
+                        bgcolor: `${theme.palette.neutral.n900}`,
+                        color: `${theme.palette.common.white}`,
+                        ':hover': {
+                          bgcolor: `${theme.palette.neutral.n900}`, // Hover background color
+                          color: `${theme.palette.common.white}`, // Hover text color
+                        },
+                      }}
+                      onClick={openEditProfileModal}
+                    >
+                      Edit Profile
+                    </Button>
+                  </div>
+                ) : (
+                  <div></div>
+                )}
               </div>
             </div>
             {/*Bargains div*/}
-            {!isPublic ? <div className="flex w-full grow flex-col gap-6">
-              <CreateDiscountCard />
-              <div className="flex h-2/5 w-full xxl-max:hidden xl-max:hidden lg-max:hidden">
-                <a className="min-w-full" href="/addbenefit">
-                  <div className='flex rounded-[32px] bg-[url("/profilebanner-sm.svg")] bg-cover py-5 sm-max:py-[10%]'>
-                    <div className="flex flex-col items-start gap-2 px-4 pb-5 pt-8">
-                      <h1 className="text-xl font-medium text-[#F6FF82] sm-max:text-3xl">
-                        Booty Call <br /> For Bargains!
-                      </h1>
-                      <Button
-                        endIcon={<BlueArrowForward />}
-                        variant="contained"
-                        className="rounded-[32px] border-2 border-white bg-[#F6FF82] text-sm font-medium normal-case text-[#8E94E9] sm-max:text-lg"
-                      >
-                        Share your discount
-                      </Button>
+            {!isPublic ? (
+              <div className="flex w-full grow flex-col gap-6">
+                <CreateDiscountCard />
+                <div className="flex h-2/5 w-full xxl-max:hidden xl-max:hidden lg-max:hidden">
+                  <a className="min-w-full" href="/addbenefit">
+                    <div className='flex rounded-[32px] bg-[url("/profilebanner-sm.svg")] bg-cover py-5 sm-max:py-[10%]'>
+                      <div className="flex flex-col items-start gap-2 px-4 pb-5 pt-8">
+                        <h1 className="text-xl font-medium text-[#F6FF82] sm-max:text-3xl">
+                          Booty Call <br /> For Bargains!
+                        </h1>
+                        <Button
+                          endIcon={<BlueArrowForward />}
+                          variant="contained"
+                          className="rounded-[32px] border-2 border-white bg-[#F6FF82] text-sm font-medium normal-case text-[#8E94E9] sm-max:text-lg"
+                        >
+                          Share your discount
+                        </Button>
+                      </div>
                     </div>
-                  </div>
-                </a>
+                  </a>
+                </div>
+                <div className="flex h-2/5 gap-6">
+                  <a
+                    href="groups"
+                    className="flex h-[126px] flex-1 items-center gap-6 rounded-3xl bg-white"
+                  >
+                    <div className="mx-6 flex flex-col">
+                      <div className="text-2xl font-semibold sm-max:text-xl xs-max:text-lg xxs-max:text-lg">
+                        Private Groups
+                      </div>
+                      <div className="text-[14px] sm-max:text-[10px] xs-max:text-[10px] xxs-max:text-[10px]">
+                        Get intimate with discounts in private groups
+                      </div>
+                    </div>
+                    <div className="mx-10 flex grow flex-row-reverse">
+                      <Image
+                        src={BlueGroupIcon}
+                        alt="Group Icon"
+                        width={50}
+                        height={50}
+                      />
+                      {/* Need custom icon for it to show*/}
+                    </div>
+                  </a>
+                </div>
               </div>
-              <div className="flex h-2/5 gap-6">
-                <a
-                  href="groups"
-                  className="flex h-[126px] flex-1 items-center gap-6 rounded-3xl bg-white"
-                >
-                  <div className="mx-6 flex flex-col">
-                    <div className="text-2xl font-semibold sm-max:text-xl xs-max:text-lg xxs-max:text-lg">
-                      Private Groups
-                    </div>
-                    <div className="text-[14px] sm-max:text-[10px] xs-max:text-[10px] xxs-max:text-[10px]">
-                      Get intimate with discounts in private groups
-                    </div>
-                  </div>
-                  <div className="mx-10 flex grow flex-row-reverse">
-                    <Image
-                      src={BlueGroupIcon}
-                      alt="Group Icon"
-                      width={50}
-                      height={50}
-                    />
-                    {/* Need custom icon for it to show*/}
-                  </div>
-                </a>
-              </div>
-            </div>: <div> </div>}
+            ) : (
+              <div> </div>
+            )}
             {/*My Benefits div*/}
             <div className="my-[80px] flex h-1/5 w-full flex-col rounded-lg sm-max:my-10 xs-max:my-8 xxs-max:my-6">
               <div className="flex h-full w-full flex-col">
