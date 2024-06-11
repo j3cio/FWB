af31e19aa6d5b7f851dcb6cb2a7c07f5ed7b9e9f
Brandon Lee <brandonlee2543@hotmail.com>
2024-06-11 09:42:16 -0400

    changed our conditional rendering statement

--- fwb/components/ui/profile/Benefits.tsx
+++ fwb/components/ui/profile/Benefits.tsx
@@ -30,7 +30,7 @@ const Benefits = async () => {
 
   return (
     <div>
-      {filteredDiscountData && filteredDiscountData.length > 0 ? (
+      {filteredDiscountData.length > 0 ? (
         <div className="flex w-full justify-center">
           <div className="flex flex-wrap justify-start gap-4 pl-2">
             {filteredDiscountData.map((company: any, index: React.Key) => (
