0bc90c51ce9481a931066f8d778ff40e541c26c6
Brandon Lee <brandonlee2543@hotmail.com>
2024-06-23 23:23:11 -0400

    Minor refactor for readability

--- fwb/__tests__/ExplorePageContent.test.tsx
+++ fwb/__tests__/ExplorePageContent.test.tsx
@@ -1,27 +1,13 @@
 /**
  * @jest-environment jsdom
  */
-import React, { Dispatch, SetStateAction } from 'react'
 import { act } from 'react-dom/test-utils'
 import 'whatwg-fetch'
 
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import ExplorePageContent from '@/components/ui/explore/ExplorePageContent'
-
-import {
-  useSearchQuery,
-  useSearchResults,
-  useSetSearchResults,
-  useSearchIndex,
-  useSetSearchIndex,
-} from '../components/hooks/SearchQuery'
-
-const mockData = [
-  { id: 1, name: 'Company A', description: 'Description for Company A' },
-  { id: 2, name: 'Company B', description: 'Description for Company B' },
-  // We'll make this more detailed once we update our db schema
-]
+import { mockData } from '@/lib/tests/mockData'
 
 // Mock any required dependencies or contexts
 class MockResponse extends Response {
@@ -70,31 +56,9 @@ jest.mock('../components/ui/explore/utils/fetchData', () => ({
 
 jest.mock('../components/hooks/SearchQuery', () => ({
   useSearchQuery: jest.fn(() => 'mock-search-query'),
-  useSearchResults: jest.fn(() => [
-    {
-      id: 1,
-      name: 'Mock Company A',
-      description: 'Mock description for Company A',
-    },
-    {
-      id: 2,
-      name: 'Mock Company B',
-      description: 'Mock description for Company B',
-    },
-  ]),
+  useSearchResults: jest.fn(() => mockData),
   useSetSearchResults: jest.fn((searchResults: any[]) => {}),
-  useSearchIndex: jest.fn(() => [
-    {
-      id: 1,
-      name: 'Mock Company A',
-      description: 'Mock description for Company A',
-    },
-    {
-      id: 2,
-      name: 'Mock Company B',
-      description: 'Mock description for Company B',
-    },
-  ]),
+  useSearchIndex: jest.fn(() => mockData),
   useSetSearchIndex: jest.fn(() => (searchIndex: any[]) => {}),
 }))
 
---
+++ fwb/lib/tests/mockData.ts
@@ -0,0 +1,5 @@
+export const mockData = [
+  { id: 1, name: 'Company A', description: 'Description for Company A' },
+  { id: 2, name: 'Company B', description: 'Description for Company B' },
+  // We'll make this more detailed once we update our db schema
+]
