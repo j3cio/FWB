b8336d145d1ce198de0cf25037ac8000b2958ab0
Derick Young <young.derickj@gmail.com>
2024-06-25 16:51:29 -0600

    Edit /group/[groupo_id] page to load user data and group data with new db

--- fwb/app/(auth)/(routes)/groups/[group_id]/page.tsx
+++ fwb/app/(auth)/(routes)/groups/[group_id]/page.tsx
@@ -66,7 +66,6 @@ async function getGroupData(params: {
 async function getUser(user_id: string) {
   const bearer_token = await auth().getToken({ template: 'testing_template' })
   const supabase_jwt = await auth().getToken({ template: 'supabase' })
-
   if (!supabase_jwt) {
     console.warn('Not signed in')
     return
@@ -98,8 +97,8 @@ async function getUser(user_id: string) {
 }
 
 async function getAllUserData(user_ids: string[]) {
-  const promises = user_ids.map((user_id: string, key: number) =>
-    getUser(user_id)
+  const promises = user_ids.map((user: any, key: number) =>
+    getUser(user.user_id)
   )
   const results = await Promise.all(promises)
 
@@ -109,29 +108,65 @@ async function getAllUserData(user_ids: string[]) {
   return filteredResults
 }
 
+
+async function getGroupsToUserTable(bearer_token: string,
+  supabase_jwt: string, group_id: string) {
+    const userId = await auth().userId
+    if (!supabase_jwt) {
+      console.log('Not signed in')
+      return
+    }
+    var myHeaders = new Headers()
+    myHeaders.append('supabase_jwt', supabase_jwt)
+    myHeaders.append('Authorization', `Bearer ${bearer_token}`)
+  
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+    }
+  
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/groupToUser?group_id=${group_id}`,
+        requestOptions
+      )
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+      const result = await response.json()
+      return result // This returns the result object
+    } catch (error) {
+      console.error('Error fetching data: ', error)
+      throw error // This re-throws the error to be handled by the caller
+    }
+
+}
+
 const Page = async ({ params }: { params: { group_id: string } }) => {
   const bearer_token = await auth().getToken({ template: 'testing_template' })
   const supabase_jwt = await auth().getToken({ template: 'supabase' })
   const groupData = await getGroupData(params)
-  const userData: any = await getAllUserData(groupData.data[0].users)
+  const userIdArray = await getGroupsToUserTable(bearer_token!, supabase_jwt!, groupData.data[0].id)
+  const userData: any = await getAllUserData(userIdArray.groups)
+  // const userData: any = await getAllUserData(groupData.data[0].users)
 
-  // Makes sure that our tokens exist, else gives us an empty array
-  const discountData: DiscountData[] =
-    bearer_token && supabase_jwt
-      ? await getAllDiscountsData(
-          groupData.data[0].discounts,
-          bearer_token,
-          supabase_jwt
-        )
-      : []
+  // // Makes sure that our tokens exist, else gives us an empty array
+  // const discountData: DiscountData[] =
+  //   bearer_token && supabase_jwt
+  //     ? await getAllDiscountsData(
+  //         groupData.data[0].discounts,
+  //         bearer_token,
+  //         supabase_jwt
+  //       )
+  //     : []
 
   return (
     <Box sx={{ backgroundColor: '#1A1A23', minHeight: '100vh' }}>
-      <GroupContent
+      {/* <GroupContent
         userData={userData}
         groupData={groupData}
         discountData={discountData}
-      />
+      /> */}
     </Box>
   )
 }
