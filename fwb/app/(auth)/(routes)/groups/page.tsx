6ae64be36ed758d632e7db6990d02a1350eb0489
Chloe-990123 <118889541+Chloe-990123@users.noreply.github.com>
2024-07-08 23:44:24 +0900

    yet another comment

--- fwb/app/(auth)/(routes)/groups/page.tsx
+++ fwb/app/(auth)/(routes)/groups/page.tsx
@@ -107,6 +107,7 @@ async function getGroupData(groupId: string) {
         throw new Error(`HTTP error! status: ${response.status}`)
       }
       const result = await response.json()
+      console.log(result)
       return result // This returns the result object
     } catch (error) {
       console.error('Error fetching data: ', error)
