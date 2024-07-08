152b67e7661c533fd9faba0aa04e58c67bb45b20
Chloe-990123 <118889541+Chloe-990123@users.noreply.github.com>
2024-07-08 21:51:57 +0900

    linked group creation to test_groups and UserToGroups

--- fwb/app/api/groups/utils/group_utils.ts
+++ fwb/app/api/groups/utils/group_utils.ts
@@ -19,12 +19,12 @@ const insertGroup = async (request: NextRequest) => {
       const formData = await request.formData()
       const newGroup = {
         name: formData.get('name'),
-        users: [formData.get('users')] || [],
-        discounts: [formData.get('discounts')] || [],
+        //users: [formData.get('users')] || [],
+        //discounts: [formData.get('discounts')] || [],
         admins: [formData.get('admins')] || [],
         public: formData.get('public') === 'false' ? false : true, // There might be a way to use a boolean rather than having to type false?
         description: formData.get('description'),
-        filePath: formData.get('filePath'),
+        //filePath: formData.get('filePath'),
       }
       const supabase = await supabaseClient(request.headers.get('supabase_jwt'))
       if (!supabase) {
@@ -33,9 +33,9 @@ const insertGroup = async (request: NextRequest) => {
           { status: 401 }
         )
       }
-      // Insert the new group into the groups table in supabase
+      // Insert the new group into the test_groups table in supabase
       const { data, error } = await supabase
-        .from('groups')
+        .from('test_groups')
         .insert([newGroup])
         .select()
 
