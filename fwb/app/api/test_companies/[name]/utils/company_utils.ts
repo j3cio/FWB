7d95c5b8bd9516ae69042799625d4ef6eec2dd40
Derick Young <young.derickj@gmail.com>
2024-07-31 10:14:05 -0600

    Create endpoint to get a single company

---
+++ fwb/app/api/test_companies/[name]/utils/company_utils.ts
@@ -0,0 +1,50 @@
+import supabaseClient from '@/supabase'
+import { auth, currentUser } from '@clerk/nextjs'
+import { NextRequest, NextResponse } from 'next/server'
+
+/**
+ * Retrieves users registered through Clerk.
+ *
+ * @param request - The NextRequest object containing the query parameters.
+ * @returns A NextResponse object containing the fetched users or an error response.
+ */
+const getSingleCompany = async (request: NextRequest) => {
+  //Extract the clerk user_id from the url path
+  const urlObject = new URL(request.url)
+  const userIdPathVariable = urlObject.pathname.split('/').pop()
+
+  try {
+    const { userId, getToken } = auth()
+    const user = await currentUser()
+    // If the user is logged in, fetch all other users on the platform
+    if (userId && user) {
+      const supabase = await supabaseClient(request.headers.get('supabase_jwt'))
+      if (!supabase) {
+        return NextResponse.json(
+          { error: 'Could not create supabase access token' },
+          { status: 401 }
+        )
+      }
+
+      let { data: company, error } = await supabase
+        .from('test_companies')
+        .select('*')
+        .eq('name', userIdPathVariable)
+
+      if (error) {
+        return NextResponse.json(
+          { error: 'Failed to fetch users' },
+          { status: 500 }
+        )
+      }
+      return NextResponse.json({ success: true, company }, { status: 200 })
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    )
+  }
+}
+
+export { getSingleCompany }
